1b1147e8fb84df707e7fefbd5d02b5585052ecfb
Wilmer Paulino <wilmer.paulino@gmail.com>
2022-05-06 12:53:04 -0700

    mssmt: Add CompressedProof serialization
    We don't add one for the decompressed variant as there's no reason to
    serialize those when the compressed variant can yield a more efficient
    serialization.
    
    The serialization scheme for CompressedProof is as follows:
    
    * Nodes:
      * Big endian encoding of the number of nodes present as uint16
      * Each node:
        * 32 byte key
        * Big endian encoding of the node sum as uint64
    * Bits:
      * 32 bytes of the compressed bits (256 bits total)

---
+++ mssmt/encoding_test.go
@@ -0,0 +1,65 @@
+package mssmt
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func assertEqualProof(t *testing.T, expected, actual *Proof) {
+	t.Helper()
+
+	for i, node := range expected.Nodes {
+		other := actual.Nodes[i]
+		require.True(t, IsEqualNode(node, other))
+	}
+}
+
+func assertEqualCompressedProof(t *testing.T, expected, actual *CompressedProof) {
+	t.Helper()
+
+	for i, node := range expected.Nodes {
+		other := actual.Nodes[i]
+		require.True(t, IsEqualNode(node, other))
+	}
+	require.Equal(t, expected.Bits, actual.Bits)
+}
+
+func TestBitPacking(t *testing.T) {
+	t.Parallel()
+
+	// Odd number of bits and greater than a byte to test edge case.
+	bits := []bool{true, true, false, false, true, false, true, true, false}
+	decompressedBits := unpackBits(packBits(bits))
+
+	// Bits up to the expected length should match.
+	require.Equal(t, bits, decompressedBits[:len(bits)])
+
+	// Remaining bits should not be set.
+	for _, isBitSet := range decompressedBits[len(bits):] {
+		require.False(t, isBitSet)
+	}
+}
+
+func TestProofEncoding(t *testing.T) {
+	t.Parallel()
+
+	tree, leaves := randTree(10_000)
+	for key := range leaves {
+		proof := tree.MerkleProof(key)
+		compressed := proof.Compress()
+
+		var buf bytes.Buffer
+		err := compressed.Encode(&buf)
+		require.NoError(t, err)
+
+		var decodedCompressed CompressedProof
+		err = decodedCompressed.Decode(bytes.NewReader(buf.Bytes()))
+		require.NoError(t, err)
+		assertEqualCompressedProof(t, compressed, &decodedCompressed)
+
+		decodedProof := decodedCompressed.Decompress()
+		assertEqualProof(t, proof, decodedProof)
+	}
+}
