3613f3dcdcef99c14fb67384e500d304b54f567f
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2022-06-29 00:43:41 -0400

    address: separate spend creation and validation

--- address/address.go
+++ address/address.go
@@ -103,6 +103,52 @@ type AddressTLV struct {
 	Type asset.Type
 }
 
+// PotentialSpend stores the information needed to check that an asset
+// spend is possible, and prepare new asset leaves to be validated with the
+// Taro VM.
+type PotentialSpend struct {
+	// InputAsset is the Asset being used by the sender.
+	InputAsset *asset.Asset
+
+	// NewAsset is the Asset that will be spent to the receiver. It may contain
+	// a split commitment, and does not have a valid witness.
+	NewAsset *asset.Asset
+
+	// InputAssets maps asset PrevIDs to Assets being spent by the sender.
+	InputAssets commitment.InputSet
+
+	// SplitAssets maps split locators to split Assets, which is used by the
+	// Taro VM when validating a spend with a split commitment.
+	// NOTE: This is nil unless the InputAsset is being split.
+	SplitAssets commitment.SplitSet
+
+	// SplitCommitment contains all data needed to validate and commit to an
+	// asset split.
+	// NOTE: This is nil unless the InputAsset is being split.
+	SplitCommitment *commitment.SplitCommitment
+
+	// ChangeLocator identifies the asset split that is asset change, to be
+	// spent back to the sender.
+	// NOTE: This is nil unless the InputAsset is being split.
+	ChangeLocator *commitment.SplitLocator
+
+	// ReceiverLocator identifies the asset split that is to be
+	// spent to the receiver.
+	// NOTE: This is nil unless the InputAsset is being split.
+	ReceiverLocator *commitment.SplitLocator
+}
+
+// A collection of structs created by validating a potential spend, that are
+// used to commit to the spend in a Bitcoin transaction.
+type ValidatedSpend struct {
+	InputAsset      *asset.Asset
+	inputCommitment *commitment.TaroCommitment
+	validatedAsset  *asset.Asset
+	SplitCommitment *commitment.SplitCommitment
+	ChangeLocator   *commitment.SplitLocator
+	ReceiverLocator *commitment.SplitLocator
+}
+
 // New creates an address for receiving a Taro asset.
 func New(id asset.ID, familyKey *btcec.PublicKey, scriptKey btcec.PublicKey,
 	internalKey btcec.PublicKey, amount uint64, assetType asset.Type, hrp string,
@@ -170,100 +216,151 @@ func (a *AddressTLV) TaprootScript() ([]byte, error) {
 		Script()
 }
 
+// prepareAssetSpend verifies that a TaroCommitment and previous input
+// controlled by the sender can satisfy a Taro address, and if so computes
+// the necessary new Asset or split commitment to complete the asset transfer.
+// The new Asset leaf or split commitment need to be signed and validated
+// before being spent.
+func prepareAssetSpend(addr AddressTLV, prevInput asset.PrevID,
+	prevTaroTree *commitment.TaroCommitment, changeIndex,
+	receiverIndex uint32) (*PotentialSpend, error) {
+
+	// Sanity check that our input could be used for this Taro address.
+	// If so, extract the input Asset to use as a template for the new Asset
+	// or input for a split commitment.
+	prevAsset, err := isValidInput(prevTaroTree, prevInput.ScriptKey, addr)
+	if err != nil {
+		return nil, err
+	}
+	newSpend := PotentialSpend{InputAsset: prevAsset}
+
+	// If our asset is a Collectible, or a Normal asset where the requested
+	// amount exactly matches the input amount, we don't need a split.
+	if addr.Type == asset.Collectible || addr.Amount == prevAsset.Amount {
+		newSpend.NewAsset = prevAsset.Copy()
+		newSpend.NewAsset.ScriptKey = addr.ScriptKey
+		newSpend.NewAsset.PrevWitnesses = []asset.Witness{{
+			PrevID:          &prevInput,
+			TxWitness:       nil,
+			SplitCommitment: nil,
+		}}
+		newSpend.InputAssets = commitment.InputSet{prevInput: prevAsset}
+		return &newSpend, nil
+	}
+
+	// If the transfer requires an asset split, the details of each split asset
+	// must be set before computing the SplitCommitment.
+	changeLocator := commitment.SplitLocator{
+		OutputIndex: changeIndex,
+		AssetID:     addr.ID,
+		ScriptKey:   prevInput.ScriptKey,
+		Amount:      prevAsset.Amount - addr.Amount,
+	}
+	receiverLocator := commitment.SplitLocator{
+		OutputIndex: receiverIndex,
+		AssetID:     addr.ID,
+		ScriptKey:   addr.ScriptKey,
+		Amount:      addr.Amount,
+	}
+	splitCommitment, err := commitment.NewSplitCommitment(
+		prevAsset, prevInput.OutPoint, &changeLocator, &receiverLocator,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// The split locators returned along with the SplitCommitment are needed
+	// to place each split asset in the correct Bitcoin output.
+	newSpend.NewAsset = splitCommitment.RootAsset
+	newSpend.InputAssets = splitCommitment.PrevAssets
+	newSpend.SplitAssets = splitCommitment.SplitAssets
+	newSpend.SplitCommitment = splitCommitment
+	newSpend.ChangeLocator = &changeLocator
+	newSpend.ReceiverLocator = &receiverLocator
+	return &newSpend, nil
+}
+
+func validateAssetSpend(privKey *btcec.PrivateKey,
+	newSpend *PotentialSpend) (*asset.Asset, error) {
+
+	// Sign a witness for the asset transfer, and create a new Asset
+	// with the witness attached to use for validation.
+	virtualTx, _, err := vm.VirtualTx(newSpend.NewAsset, newSpend.InputAssets)
+	if err != nil {
+		return nil, err
+	}
+
+	newWitness, err := signVirtualKeySpend(
+		*privKey, virtualTx, newSpend.InputAsset, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	validatedAsset := newSpend.NewAsset.Copy()
+	validatedAsset.PrevWitnesses[0].TxWitness = *newWitness
+
+	// If the transfer contains no asset splits, we only need to validate
+	// the new asset with its witness attached.
+	if newSpend.SplitCommitment == nil {
+		vm, err := vm.New(validatedAsset, nil, newSpend.InputAssets)
+		if err != nil {
+			return nil, err
+		}
+		if vm.Execute() != nil {
+			return nil, err
+		}
+		return validatedAsset, nil
+	}
+
+	// If the transfer includes an asset split, we have to validate each
+	// split asset to ensure that our new Asset is committing to
+	// a valid SplitCommitment.
+	for _, splitAsset := range newSpend.SplitAssets {
+		vm, err := vm.New(validatedAsset, splitAsset, newSpend.InputAssets)
+		if err != nil {
+			return nil, err
+		}
+		if vm.Execute() != nil {
+			return nil, err
+		}
+	}
+	return validatedAsset, nil
+}
+
 // CreateAssetSpend computes the asset leaf and split commitment needed to
 // send assets to the receiver from the specified input. This is done by first
 // validating that the input asset can satisfy the given Taro address. A split
 // commitment is generated if needed, and the transfer is validated using the
 // Taro VM. The returned asset leaves and split commitment should be used to
 // construct new Taro commitments for both sender and receiver.
-func (a AddressTLV) CreateAssetSpend(privKey btcec.PrivateKey,
-	input asset.PrevID, inputCommitment *commitment.TaroCommitment) (
-	*asset.Asset, *asset.Asset, *commitment.SplitCommitment, error,
-) {
-	inputAsset, err := a.isValidInput(inputCommitment, input.ScriptKey)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	// To validate the transfer, we need the input and output assets, and
-	// optionally a split commitment plus all created splits.
+func CreateAssetSpend(privKey *btcec.PrivateKey, input asset.PrevID,
+	inputCommitment *commitment.TaroCommitment, changeIdx, receiverIdx uint32,
+	addr AddressTLV) (*ValidatedSpend, error) {
 
-	var (
-		splitCommitment *commitment.SplitCommitment
-		newAsset        *asset.Asset
-		inputAssets     commitment.InputSet
-		splitAssets     commitment.SplitSet
-		splitErr        error
+	// Check for validity of the input and compute the data needed to validate
+	// the asset transfer.
+	newSpend, err := prepareAssetSpend(
+		addr, input, inputCommitment, changeIdx, receiverIdx,
 	)
-
-	// If our asset is a Collectible, or a Normal asset where the requested
-	// amount exactly matches the input amount, we don't need a split.
-	if a.Type == asset.Collectible || a.Amount == inputAsset.Amount {
-		newAsset = inputAsset.Copy()
-		newAsset.ScriptKey = a.ScriptKey
-		newAsset.PrevWitnesses = []asset.Witness{{
-			PrevID:          &input,
-			TxWitness:       nil,
-			SplitCommitment: nil,
-		}}
-		inputAssets = commitment.InputSet{input: inputAsset}
-	} else {
-		// NOTE: Default to using the first output for asset change,
-		// and the second output for the receiver.
-		changeLocator := commitment.SplitLocator{
-			OutputIndex: 0,
-			AssetID:     a.ID,
-			ScriptKey:   input.ScriptKey,
-			Amount:      inputAsset.Amount - a.Amount,
-		}
-		receiverLocator := commitment.SplitLocator{
-			OutputIndex: 1,
-			AssetID:     a.ID,
-			ScriptKey:   a.ScriptKey,
-			Amount:      a.Amount,
-		}
-		splitCommitment, splitErr = commitment.NewSplitCommitment(
-			inputAsset, input.OutPoint, &changeLocator, &receiverLocator,
-		)
-		if splitErr != nil {
-			return nil, nil, nil, splitErr
-		}
-		newAsset = splitCommitment.RootAsset
-		inputAssets = splitCommitment.PrevAssets
-		splitAssets = splitCommitment.SplitAssets
+	if err != nil {
+		return nil, err
 	}
+
 	// With the desired output assets and split commitment, sign a witness
-	// for the transfer and validate with the Taro VM.
-	virtualTx, _, err := vm.VirtualTx(newAsset, inputAssets)
+	// for the transfer and validate with the Taro VM. If the transfer is
+	// valid, the Asset is updated to contain the new witness.
+	validatedAsset, err := validateAssetSpend(privKey, newSpend)
 	if err != nil {
-		return nil, nil, nil, err
+		return nil, err
 	}
-	newWitness, err := signVirtualKeySpend(
-		privKey, virtualTx, inputAsset, 0,
-	)
-	if err != nil {
-		return nil, nil, nil, err
-	}
-	newAsset.PrevWitnesses[0].TxWitness = *newWitness
-	if len(splitAssets) == 0 {
-		vm, err := vm.New(newAsset, nil, inputAssets)
-		if err != nil {
-			return nil, nil, nil, err
-		}
-		if vm.Execute() != nil {
-			return nil, nil, nil, err
-		}
-	} else {
-		for _, splitAsset := range splitAssets {
-			vm, err := vm.New(newAsset, splitAsset, inputAssets)
-			if err != nil {
-				return nil, nil, nil, err
-			}
-			if vm.Execute() != nil {
-				return nil, nil, nil, err
-			}
-		}
-	}
-	return inputAsset, newAsset, splitCommitment, nil
+
+	// Pass along the data needed to create new Taro commitment trees and
+	// create the final Bitcoin transaction including the asset transfer.
+	validSpend := ValidatedSpend{newSpend.InputAsset, inputCommitment,
+		validatedAsset, newSpend.SplitCommitment, newSpend.ChangeLocator,
+		newSpend.ReceiverLocator}
+
+	return &validSpend, nil
 }
 
 // isValidInput verifies that the Taro commitment of the input contains an
