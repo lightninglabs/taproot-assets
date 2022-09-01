9bbf154551a07ef6eaa73a282923ea8b8c016c86
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2022-09-01 15:26:44 -0400

    taroscript: add areValidIndexes to check TX indexes

--- taroscript/send.go
+++ taroscript/send.go
@@ -1,10 +1,14 @@
 package taroscript
 
 import (
+	"errors"
+	"sort"
+
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/lightninglabs/taro/address"
 	"github.com/lightninglabs/taro/asset"
 	"github.com/lightninglabs/taro/commitment"
+	"golang.org/x/exp/maps"
 )
 
 var (
@@ -22,8 +26,49 @@ var (
 		"send: Input asset value is insufficient",
 	)
 
+	// ErrInvalidOutputIndexes is an error returned when we attempt to spend
+	// to Bitcoin output indexes that do not start at 0 or
+	// are not continuous.
+	ErrInvalidOutputIndexes = errors.New(
+		"send: Output indexes not starting at 0 and continuous",
 )
 
+)
+
+// SpendLocators stores a split locators for each receiver, keyed by their
+// AssetCommitmentKey. These locators are used to create split commitments and
+// the final PSBT for the transfer. AssetCommitmentKeys are unique to each asset
+// and each receiver due to the inclusion of the receiver's ScriptKey.
+type SpendLocators = map[[32]byte]commitment.SplitLocator
+
+// areValidIndexes checks a set of split locators to check for the minimum
+// number of locators, and tests if the locators could be used for a Taro-only
+// spend, i.e. a TX that does not need other outputs added to be valid.
+func areValidIndexes(locators SpendLocators) (bool, error) {
+	// Sanity check the output indexes provided by the sender. There must be
+	// at least two indexes; one for the receiver, and one for the change
+	// commitment for the sender.
+	idxCount := len(locators)
+	if idxCount < 2 {
+		return false, ErrInvalidOutputIndexes
+	}
+
+	// If the indexes start from 0 and form a continuous range, then the
+	// resulting TX would be valid without any changes (Taro-only spend).
+	taroOnlySpend := true
+	txoLocators := maps.Values(locators)
+	sort.Slice(txoLocators, func(i, j int) bool {
+		return txoLocators[i].OutputIndex < txoLocators[j].OutputIndex
+	})
+	for i := uint32(0); i < uint32(idxCount); i++ {
+		if txoLocators[i].OutputIndex != i {
+			taroOnlySpend = false
+			break
+		}
+	}
+
+	return taroOnlySpend, nil
+}
 
 // isValidInput verifies that the Taro commitment of the input contains an
 // asset that could be spent to the given Taro address.
--- taroscript/send_test.go
+++ taroscript/send_test.go
@@ -5,12 +5,59 @@ import (
 	"testing"
 
 	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/lightninglabs/taro/address"
 	"github.com/lightninglabs/taro/asset"
 	"github.com/lightninglabs/taro/commitment"
 	"github.com/lightninglabs/taro/mssmt"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/stretchr/testify/require"
+)
+
+// spendData represents the collection of structs needed to begin a spend.
+type spendData struct {
+	collectAmt                     uint64
+	normalAmt1                     uint64
+	normalAmt2                     uint64
+	genesis1                       asset.Genesis
+	genesis1collect                asset.Genesis
+	spenderPrivKey                 btcec.PrivateKey
+	spenderPubKey                  btcec.PublicKey
+	spenderScriptKey               btcec.PublicKey
+	spenderDescriptor              keychain.KeyDescriptor
+	receiverPrivKey                btcec.PrivateKey
+	receiverPubKey                 btcec.PublicKey
+	familyKey                      asset.FamilyKey
+	address1                       address.Taro
+	address1CollectFamily          address.Taro
+	address2                       address.Taro
+	address1StateKey               [32]byte
+	address1CollectFamilyStateKey  [32]byte
+	address2StateKey               [32]byte
+	asset1                         asset.Asset
+	asset1CollectFamily            asset.Asset
+	asset2                         asset.Asset
+	asset1PrevID                   asset.PrevID
+	asset1CollectFamilyPrevID      asset.PrevID
+	asset2PrevID                   asset.PrevID
+	asset1InputAssets              commitment.InputSet
+	asset1CollectFamilyInputAssets commitment.InputSet
+	asset2InputAssets              commitment.InputSet
+	asset1TaroTree                 commitment.TaroCommitment
+	asset1CollectFamilyTaroTree    commitment.TaroCommitment
+	asset2TaroTree                 commitment.TaroCommitment
+}
+
+var (
+	key1Bytes, _ = hex.DecodeString(
+		"a0afeb165f0ec36880b68e0baabd9ad9c62fd1a69aa998bc30e9a346202e" +
+			"078e",
+	)
+	key2Bytes, _ = hex.DecodeString(
+		"a0afeb165f0ec36880b68e0baabd9ad9c62fd1a69aa998bc30e9a346202e" +
+			"078d",
+	)
 )
 
 func randKey(t *testing.T) *btcec.PrivateKey {
@@ -44,6 +91,164 @@ func randFamilyKey(t *testing.T, genesis asset.Genesis) *asset.FamilyKey {
 	require.NoError(t, err)
 
 	return familyKey
+}
+
+func initSpendScenario(t *testing.T) spendData {
+	t.Helper()
+
+	// Amounts and genesises, needed for addresses and assets. We need both
+	// a normal and collectible asset, and three amounts to test splits.
+	state := spendData{
+		collectAmt:      1,
+		normalAmt1:      2,
+		normalAmt2:      5,
+		genesis1:        randGenesis(t, asset.Normal),
+		genesis1collect: randGenesis(t, asset.Collectible),
+	}
+
+	// Keys for sender, receiver, and family. Default to keypath spend
+	// for the spender ScriptKey.
+	spenderPrivKey, spenderPubKey := btcec.PrivKeyFromBytes(key1Bytes)
+	state.spenderPrivKey = *spenderPrivKey
+	state.spenderPubKey = *spenderPubKey
+	spenderScriptKey := *txscript.ComputeTaprootKeyNoScript(
+		&state.spenderPubKey,
+	)
+	state.spenderScriptKey = spenderScriptKey
+	state.spenderDescriptor = keychain.KeyDescriptor{
+		PubKey: &state.spenderScriptKey,
+	}
+	receiverPrivKey, receiverPubKey := btcec.PrivKeyFromBytes(key2Bytes)
+	state.receiverPrivKey = *receiverPrivKey
+	state.receiverPubKey = *receiverPubKey
+	familyKey := randFamilyKey(t, state.genesis1collect)
+	state.familyKey = *familyKey
+
+	// Addesses to cover both asset types and all three asset values.
+	// Store the receiver StateKeys as well.
+	address1, err := address.New(
+		state.genesis1.ID(), nil, state.receiverPubKey,
+		state.receiverPubKey, state.normalAmt1,
+		asset.Normal, &address.MainNetTaro,
+	)
+	require.NoError(t, err)
+	state.address1 = *address1
+	state.address1StateKey = state.address1.AssetCommitmentKey()
+
+	address1CollectFamily, err := address.New(
+		state.genesis1collect.ID(), &state.familyKey.FamKey,
+		state.receiverPubKey, state.receiverPubKey, state.collectAmt,
+		asset.Collectible, &address.TestNet3Taro,
+	)
+	require.NoError(t, err)
+	state.address1CollectFamily = *address1CollectFamily
+	state.address1CollectFamilyStateKey = state.address1CollectFamily.
+		AssetCommitmentKey()
+
+	address2, err := address.New(
+		state.genesis1.ID(), nil, state.receiverPubKey,
+		state.receiverPubKey, state.normalAmt2,
+		asset.Normal, &address.MainNetTaro,
+	)
+	require.NoError(t, err)
+	state.address2 = *address2
+	state.address2StateKey = state.address2.AssetCommitmentKey()
+
+	// Generate matching assets and PrevIDs.
+	updateScenarioAssets(t, &state)
+
+	// Generate matching TaroCommitments.
+	updateScenarioCommitments(t, &state)
+
+	return state
+}
+
+func updateScenarioAssets(t *testing.T, state *spendData) {
+	t.Helper()
+
+	require.NotNil(t, state)
+
+	locktime := uint64(1)
+	relLocktime := uint64(1)
+
+	// Assets to cover both asset types and all three asset values.
+	asset1, err := asset.New(
+		state.genesis1, state.normalAmt1, locktime,
+		relLocktime, state.spenderDescriptor, nil,
+	)
+	require.NoError(t, err)
+	state.asset1 = *asset1
+
+	asset1CollectFamily, err := asset.New(
+		state.genesis1collect, state.collectAmt, locktime,
+		relLocktime, state.spenderDescriptor, &state.familyKey,
+	)
+	require.NoError(t, err)
+	state.asset1CollectFamily = *asset1CollectFamily
+
+	asset2, err := asset.New(
+		state.genesis1, state.normalAmt2, locktime,
+		relLocktime, state.spenderDescriptor, nil,
+	)
+	require.NoError(t, err)
+	state.asset2 = *asset2
+
+	// Asset PrevIDs, required to represent an input asset for a spend.
+	state.asset1PrevID = asset.PrevID{
+		OutPoint:  wire.OutPoint{},
+		ID:        state.asset1.ID(),
+		ScriptKey: state.spenderScriptKey,
+	}
+	state.asset1CollectFamilyPrevID = asset.PrevID{
+		OutPoint:  wire.OutPoint{},
+		ID:        state.asset1CollectFamily.ID(),
+		ScriptKey: state.spenderScriptKey,
+	}
+	state.asset2PrevID = asset.PrevID{
+		OutPoint:  wire.OutPoint{},
+		ID:        state.asset2.ID(),
+		ScriptKey: state.spenderScriptKey,
+	}
+
+	state.asset1InputAssets = commitment.InputSet{
+		state.asset1PrevID: &state.asset1,
+	}
+	state.asset1CollectFamilyInputAssets = commitment.InputSet{
+		state.asset1CollectFamilyPrevID: &state.asset1CollectFamily,
+	}
+	state.asset2InputAssets = commitment.InputSet{
+		state.asset2PrevID: &state.asset2,
+	}
+}
+
+func updateScenarioCommitments(t *testing.T, state *spendData) {
+	t.Helper()
+
+	require.NotNil(t, state)
+
+	// TaroCommitments for each asset.
+	asset1AssetTree, err := commitment.NewAssetCommitment(&state.asset1)
+	require.NoError(t, err)
+	asset1TaroTree, err := commitment.NewTaroCommitment(asset1AssetTree)
+	require.NoError(t, err)
+	state.asset1TaroTree = *asset1TaroTree
+
+	asset1CollectFamilyAssetTree, err := commitment.NewAssetCommitment(
+		&state.asset1CollectFamily,
+	)
+	require.NoError(t, err)
+	asset1CollectFamilyTaroTree, err := commitment.NewTaroCommitment(
+		asset1CollectFamilyAssetTree,
+	)
+	require.NoError(t, err)
+	state.asset1CollectFamilyTaroTree = *asset1CollectFamilyTaroTree
+
+	asset2AssetTree, err := commitment.NewAssetCommitment(&state.asset2)
+	require.NoError(t, err)
+	asset2TaroTree, err := commitment.NewTaroCommitment(asset2AssetTree)
+	require.NoError(t, err)
+	state.asset2TaroTree = *asset2TaroTree
+	require.NoError(t, err)
 }
 
 func assertAssetEqual(t *testing.T, a, b *asset.Asset) {
@@ -83,6 +288,60 @@ func assertAssetEqual(t *testing.T, a, b *asset.Asset) {
 	require.Equal(t, a.ScriptVersion, b.ScriptVersion)
 	require.Equal(t, a.ScriptKey, b.ScriptKey)
 	require.Equal(t, a.FamilyKey, b.FamilyKey)
+}
+
+// TestValidIndexes tests various sets of asset locators to assert that we can
+// detect an incomplete set of locators, and sets that form a valid Bitcoin
+// transaction.
+func TestValidIndexes(t *testing.T) {
+	t.Parallel()
+
+	state := initSpendScenario(t)
+
+	spenderStateKey := asset.AssetCommitmentKey(
+		state.asset1.ID(), &state.spenderScriptKey, true,
+	)
+	receiverStateKey := state.address1.AssetCommitmentKey()
+	receiver2StateKey := state.address2.AssetCommitmentKey()
+
+	locators := make(SpendLocators)
+
+	// Insert a locator for the sender.
+	locators[spenderStateKey] = commitment.SplitLocator{
+		OutputIndex: 0,
+	}
+
+	// Reject groups of locators smaller than 2.
+	taroOnlySpend, err := areValidIndexes(locators)
+	require.False(t, taroOnlySpend)
+	require.ErrorIs(t, err, ErrInvalidOutputIndexes)
+
+	// Insert a locator for the receiver, that would form a Taro-only spend.
+	locators[receiverStateKey] = commitment.SplitLocator{
+		OutputIndex: 1,
+	}
+
+	taroOnlySpend, err = areValidIndexes(locators)
+	require.True(t, taroOnlySpend)
+	require.NoError(t, err)
+
+	// Modify the receiver locator so the indexes are no longer continuous.
+	locators[receiverStateKey] = commitment.SplitLocator{
+		OutputIndex: 2,
+	}
+
+	taroOnlySpend, err = areValidIndexes(locators)
+	require.False(t, taroOnlySpend)
+	require.NoError(t, err)
+
+	// Check for correctness with more than 2 locators.
+	locators[receiver2StateKey] = commitment.SplitLocator{
+		OutputIndex: 1,
+	}
+
+	taroOnlySpend, err = areValidIndexes(locators)
+	require.True(t, taroOnlySpend)
+	require.NoError(t, err)
 }
 
 // TestAddressValidInput tests edge cases around validating inputs for asset
