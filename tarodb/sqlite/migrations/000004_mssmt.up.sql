9cb43760e327a564b0bc6dfdc062d3add0aa8a2c
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-09-07 18:25:33 -0700

    tarodb/smt: add namespace field to existing node table
    In this commit, we add a namespace field to the existing node table.
    This'll allow us to have multiple independent trees live on disk. An
    upcoming use case is maintaining a Universe for each set of assets we've
    minted.
    
    We also modify the primary key of the table itself to be (hash_key,
    namespace). This'll let us do things like copy over a tree to an
    entirely new namespace.

--- tarodb/sqlite/migrations/000004_mssmt.up.sql
+++ tarodb/sqlite/migrations/000004_mssmt.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS mssmt_nodes (
     -- hash_key is the hash key by which we reference all nodes.
-    hash_key BLOB PRIMARY KEY,
+    hash_key BLOB NOT NULL,
  
     -- l_hash_key is the hash key of the left child or NULL. If this is a
     -- branch then either l_hash_key or r_hash_key is not NULL.
@@ -17,7 +17,16 @@ CREATE TABLE IF NOT EXISTS mssmt_nodes (
     value BLOB,
 
     -- sum is the sum of the node.
-    sum BIGINT NOT NULL
+    sum BIGINT NOT NULL,
+
+    -- namespace allows an application to store several distinct MS-SMT nodes
+    -- in the same table, partitioning them by the namespace value.
+    namespace VARCHAR NOT NULL,
+
+    -- A combination of the hash_key and the namespace comprise our primary
+    -- key. Using these two in concert allows us to do things like copy trees
+    -- between namespaces.
+    PRIMARY KEY (hash_key, namespace)
 );
 
 CREATE INDEX IF NOT EXISTS mssmt_nodes_l_hash_key_idx ON mssmt_nodes (l_hash_key);
