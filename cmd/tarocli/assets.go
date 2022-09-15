5811cb36781bccec58b7f0664e337ab5b0c8cdf1
Olaoluwa Osuntokun <laolu32@gmail.com>
2022-09-14 21:25:53 -0700

    cmd/tarocli: add asset send CLI command

--- cmd/tarocli/assets.go
+++ cmd/tarocli/assets.go
@@ -18,6 +18,7 @@ var assetsCommands = []cli.Command{
 			mintAssetCommand,
 			listAssetsCommand,
 			listAssetBalancesCommand,
+			sendAssetsCommand,
 		},
 	},
 }
@@ -194,6 +195,37 @@ func listAssetBalances(ctx *cli.Context) error {
 	resp, err := client.ListBalances(ctxc, req)
 	if err != nil {
 		return fmt.Errorf("unable to list asset balances: %w", err)
+	}
+}
+
+var sendAssetsCommand = cli.Command{
+	Name:        "send",
+	ShortName:   "s",
+	Usage:       "send an asset",
+	Description: "send asset w/ a taro addr",
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  addrName,
+			Usage: "addr to send to",
+		},
+		// TODO(roasbeef): add arg for file name to write sender proof
+		// blob
+	},
+	Action: sendAssets,
+}
+
+func sendAssets(ctx *cli.Context) error {
+	switch {
+	case ctx.String(addrName) == "":
+		_ = cli.ShowCommandHelp(ctx, "sent")
+		return nil
+	}
+
+	resp, err := client.SendAsset(ctxc, &tarorpc.SendAssetRequest{
+		TaroAddr: ctx.String(addrName),
+	})
+	if err != nil {
+		return fmt.Errorf("unable to list assets: %w", err)
 	}
 
 	printRespJSON(resp)
