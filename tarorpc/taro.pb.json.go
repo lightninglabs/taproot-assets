416f73c878c45bb4081f5495ebd24727202b15aa
Andras Banki-Horvath <bhandras@gmail.com>
2022-09-23 10:44:51 +0200

    tarorpc: add protos for ListTransfers

--- tarorpc/taro.pb.json.go
+++ tarorpc/taro.pb.json.go
@@ -98,6 +98,31 @@ func RegisterTaroJSONCallbacks(registry map[string]func(ctx context.Context,
 		callback(string(respBytes), nil)
 	}
 
+	registry["tarorpc.Taro.ListTransfers"] = func(ctx context.Context,
+		conn *grpc.ClientConn, reqJSON string, callback func(string, error)) {
+
+		req := &ListTransfersRequest{}
+		err := marshaler.Unmarshal([]byte(reqJSON), req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		client := NewTaroClient(conn)
+		resp, err := client.ListTransfers(ctx, req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		respBytes, err := marshaler.Marshal(resp)
+		if err != nil {
+			callback("", err)
+			return
+		}
+		callback(string(respBytes), nil)
+	}
+
 	registry["tarorpc.Taro.StopDaemon"] = func(ctx context.Context,
 		conn *grpc.ClientConn, reqJSON string, callback func(string, error)) {
 
