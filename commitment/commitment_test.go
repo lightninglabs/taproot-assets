85d2feaaf69378669004d4dc575613d4165166b7
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2022-10-20 15:52:22 -0400

    commitment: Add support for collectibles in splits

--- commitment/commitment_test.go
+++ commitment/commitment_test.go
@@ -464,7 +464,7 @@ func TestSplitCommitment(t *testing.T) {
 		err  error
 	}{
 		{
-			name: "invalid asset input type",
+			name: "collectible split with excess external locators",
 			f: func() (*asset.Asset, *SplitLocator, []*SplitLocator) {
 				input := randAsset(
 					t, genesisCollectible,
@@ -473,14 +473,52 @@ func TestSplitCommitment(t *testing.T) {
 				root := &SplitLocator{
 					OutputIndex: 0,
 					AssetID:     genesisCollectible.ID(),
+					ScriptKey:   asset.NUMSCompressedKey,
+					Amount:      0,
+				}
+				external := []*SplitLocator{{
+					OutputIndex: 1,
+					AssetID:     genesisCollectible.ID(),
 					ScriptKey: asset.ToSerialized(
-						input.ScriptKey.PubKey,
+						randKey(t).PubKey(),
 					),
 					Amount: input.Amount,
-				}
-				return input, root, nil
+				}, {
+					OutputIndex: 1,
+					AssetID:     genesisCollectible.ID(),
+					ScriptKey: asset.ToSerialized(
+						randKey(t).PubKey(),
+					),
+					Amount: input.Amount,
+				}}
+				return input, root, external
 			},
-			err: ErrInvalidInputType,
+			err: ErrInvalidSplitLocatorCount,
+		},
+		{
+			name: "collectible split commitment",
+			f: func() (*asset.Asset, *SplitLocator, []*SplitLocator) {
+				input := randAsset(
+					t, genesisCollectible,
+					familyKeyCollectible,
+				)
+				root := &SplitLocator{
+					OutputIndex: 0,
+					AssetID:     genesisCollectible.ID(),
+					ScriptKey:   asset.NUMSCompressedKey,
+					Amount:      0,
+				}
+				external := []*SplitLocator{{
+					OutputIndex: 1,
+					AssetID:     genesisCollectible.ID(),
+					ScriptKey: asset.ToSerialized(
+						randKey(t).PubKey(),
+					),
+					Amount: input.Amount,
+				}}
+				return input, root, external
+			},
+			err: nil,
 		},
 		{
 			name: "locator duplicate output index",
