25d50e5b84b4759e53ca2e846e1b09bae727716c
ffranr <robyn@lightning.engineering>
2023-01-17 19:37:01 +0000

    itest: add ChainPorter event ntfs check for broadcast state execution

--- itest/send_test.go
+++ itest/send_test.go
@@ -3,8 +3,13 @@ package itest
 import (
 	"bytes"
 	"context"
+	"io"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/lightninglabs/taro/chanutils"
+	"github.com/lightninglabs/taro/tarofreighter"
 	"github.com/lightninglabs/taro/tarorpc"
 	"github.com/lightningnetwork/lnd/lntest/wait"
 	"github.com/stretchr/testify/require"
@@ -13,6 +18,30 @@ import (
 // testBasicSend tests that we can properly send assets back and forth between
 // nodes.
 func testBasicSend(t *harnessTest) {
+	var (
+		ctxb = context.Background()
+		wg   sync.WaitGroup
+	)
+
+	// Subscribe to receive assent send events from primary taro node.
+	eventNtfns, err := t.tarod.SubscribeSendAssetEventNtfns(
+		ctxb, &tarorpc.SubscribeSendAssetEventNtfnsRequest{},
+	)
+	require.NoError(t.t, err)
+
+	// Test to ensure that we execute the transaction broadcast state.
+	// This test is executed in a goroutine to ensure that we can receive
+	// the event notification from the taro node as the rest of the test
+	// proceeds.
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		assertRecvSendSateExecEvent(
+			t, eventNtfns, tarofreighter.SendStateBroadcast,
+		)
+	}()
+
 	// First, we'll make an normal assets with enough units to allow us to
 	// send it around a few times.
 	rpcAssets := mintAssetsConfirmBatch(
@@ -20,8 +49,6 @@ func testBasicSend(t *harnessTest) {
 	)
 
 	genBootstrap := rpcAssets[0].AssetGenesis.GenesisBootstrapInfo
-
-	ctxb := context.Background()
 
 	// Now that we have the asset created, we'll make a new node that'll
 	// serve as the node which'll receive the assets.
@@ -107,4 +134,65 @@ func testBasicSend(t *harnessTest) {
 		}, defaultTimeout/2)
 		require.NoError(t.t, err)
 	}
+
+	// Close event stream.
+	err = eventNtfns.CloseSend()
+	require.NoError(t.t, err)
+
+	wg.Wait()
+}
+
+// assertRecvSendSateExecEvent asserts that the given send state execution event
+// notification was received. This function will block until the event is
+// received or the event stream is closed.
+func assertRecvSendSateExecEvent(
+	t *harnessTest,
+	eventNtfns tarorpc.Taro_SubscribeSendAssetEventNtfnsClient,
+	targetSendState tarofreighter.SendState,
+) {
+
+	targetSendStateStr := targetSendState.String()
+	foundTargetState := false
+	for {
+		if foundTargetState {
+			break
+		}
+
+		event, err := eventNtfns.Recv()
+
+		// Break if we get an EOF, which means the stream was
+		// closed.
+		//
+		// Use string comparison here because the RPC protocol
+		// does not transport wrapped error structures.
+		if err != nil &&
+			strings.Contains(err.Error(), io.EOF.Error()) {
+
+			break
+		}
+
+		// If err is not EOF, then we expect it to be nil.
+		require.NoError(t.t, err)
+
+		// Check for the transaction broadcast state.
+		switch eventTyped := event.Event.(type) {
+		case *tarorpc.SendAssetEvent_ExecuteSendStateEvent:
+			executeSendStateEvent := eventTyped.ExecuteSendStateEvent
+
+			// Log send state execution.
+			timestamp := time.UnixMicro(
+				executeSendStateEvent.Timestamp,
+			)
+			t.Logf("Executing send state (%v): %v",
+				timestamp.Format(time.RFC3339Nano),
+				executeSendStateEvent.SendState,
+			)
+
+			if executeSendStateEvent.SendState == targetSendStateStr {
+				foundTargetState = true
+			}
+		}
+	}
+
+	require.True(t.t, foundTargetState)
 }
