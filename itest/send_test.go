a5a7d3a8df38a3ad8e91ce6d99737654506976b1
Oliver Gugger <gugger@gmail.com>
2023-01-25 17:46:12 +0100

    itest: refactor send assertions
    We refactor the tests in a way that includes asserting the outbound
    transfer before confirming it on chain.

--- itest/send_test.go
+++ itest/send_test.go
@@ -1,17 +1,14 @@
 package itest
 
 import (
-	"bytes"
 	"context"
 	"io"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/lightninglabs/taro/chanutils"
 	"github.com/lightninglabs/taro/tarofreighter"
 	"github.com/lightninglabs/taro/tarorpc"
-	"github.com/lightningnetwork/lnd/lntest/wait"
 	"github.com/stretchr/testify/require"
 )
 
@@ -42,18 +39,20 @@ func testBasicSend(t *harnessTest) {
 		)
 	}()
 
-	// First, we'll make an normal assets with enough units to allow us to
+	// First, we'll make a normal assets with enough units to allow us to
 	// send it around a few times.
 	rpcAssets := mintAssetsConfirmBatch(
 		t, t.tarod, []*tarorpc.MintAssetRequest{simpleAssets[0]},
 	)
 
-	genBootstrap := rpcAssets[0].AssetGenesis.GenesisBootstrapInfo
+	genInfo := rpcAssets[0].AssetGenesis
+	genBootstrap := genInfo.GenesisBootstrapInfo
 
 	// Now that we have the asset created, we'll make a new node that'll
 	// serve as the node which'll receive the assets.
 	secondTarod := setupTarodHarness(
-		t.t, t, t.lndHarness.BackendCfg, t.lndHarness.Bob, t.universeServer,
+		t.t, t, t.lndHarness.BackendCfg, t.lndHarness.Bob,
+		t.universeServer,
 	)
 	defer func() {
 		require.NoError(t.t, secondTarod.stop(true))
@@ -68,10 +67,12 @@ func testBasicSend(t *harnessTest) {
 	currentUnits := simpleAssets[0].Amount
 
 	for i := 0; i < numSends; i++ {
-		bobAddr, err := secondTarod.NewAddr(ctxb, &tarorpc.NewAddrRequest{
-			GenesisBootstrapInfo: genBootstrap,
-			Amt:                  numUnits,
-		})
+		bobAddr, err := secondTarod.NewAddr(
+			ctxb, &tarorpc.NewAddrRequest{
+				GenesisBootstrapInfo: genBootstrap,
+				Amt:                  numUnits,
+			},
+		)
 		require.NoError(t.t, err)
 
 		// Deduct what we sent from the expected current number of
@@ -82,57 +83,12 @@ func testBasicSend(t *harnessTest) {
 
 		sendResp := sendAssetsToAddr(t, t.tarod, bobAddr)
 
-		// Check that we now have two new outputs, and that they differ
-		// in outpoints and scripts.
-		outputs := sendResp.TaroTransfer.NewOutputs
-		require.Len(t.t, outputs, 2)
-
-		outpoints := make(map[string]struct{})
-		scripts := make(map[string]struct{})
-		for _, o := range outputs {
-			_, ok := outpoints[o.AnchorPoint]
-			require.False(t.t, ok)
-
-			_, ok = scripts[string(o.ScriptKey)]
-			require.False(t.t, ok)
-
-			outpoints[o.AnchorPoint] = struct{}{}
-			scripts[string(o.ScriptKey)] = struct{}{}
-		}
-
-		sendRespJSON, err := formatProtoJSON(sendResp)
-		require.NoError(t.t, err)
-		t.Logf("Got response from sending assets: %v", sendRespJSON)
-
-		// Mine a block to force the send we created above to confirm.
-		_ = mineBlocks(t, t.lndHarness, 1, len(rpcAssets))
-
-		// Confirm that we can externally view the transfer.
-		err = wait.Predicate(func() bool {
-			resp, err := t.tarod.ListTransfers(
-				ctxb, &tarorpc.ListTransfersRequest{},
-			)
-			require.NoError(t.t, err)
-			require.Len(t.t, resp.Transfers, i+1)
-
-			// Assert the new outpoint, script and amount is in the
-			// list.
-			transfer := resp.Transfers[i]
-			require.Contains(t.t, outpoints, transfer.NewAnchorPoint)
-
-			delta := transfer.AssetSpendDeltas[0]
-			require.Contains(t.t, scripts, string(delta.NewScriptKey))
-			require.Equal(t.t, currentUnits, delta.NewAmt)
-
-			sameAssetID := func(xfer *tarorpc.AssetTransfer) bool {
-				return bytes.Equal(xfer.AssetSpendDeltas[0].AssetId,
-					rpcAssets[0].AssetGenesis.AssetId)
-			}
-
-			// Check asset ID is unchanged.
-			return chanutils.All(resp.Transfers, sameAssetID)
-		}, defaultTimeout/2)
-		require.NoError(t.t, err)
+		confirmAndAssertOutboundTransfer(
+			t, t.tarod, sendResp, genInfo.AssetId, currentUnits,
+			i, i+1,
+		)
+		_ = sendProof(t, t.tarod, secondTarod, bobAddr, genInfo)
+		assertReceiveComplete(t, secondTarod, i+1)
 	}
 
 	// Close event stream.
