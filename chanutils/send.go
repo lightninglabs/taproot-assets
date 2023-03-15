5695ecc6fbb547d1fd2f608501a6b8e19156dbe8
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-03-14 22:00:02 -0700

    chanutils: add SendAll function

--- chanutils/send.go
+++ chanutils/send.go
@@ -11,3 +11,12 @@ func SendOrQuit[T any, Q any](c chan<- T, msg T, quit chan Q) bool {
 		return false
 	}
 }
+
+// SendAll attempts to send all messages through channel c.
+//
+// TODO(roasbeef): add non-blocking variant?
+func SendAll[T any](c chan<- T, msgs ...T) {
+	for _, msg := range msgs {
+		c <- msg
+	}
+}
