68e1bd81e7cf42df3cd1221c8178465d635fe1b2
ffranr <robyn@lightning.engineering>
2023-03-22 01:05:10 +0000

    commitment: generalise error message: ErrInvalidSplitAmount

--- commitment/split.go
+++ commitment/split.go
@@ -19,11 +19,9 @@ var (
 		"found locator with duplicate output index",
 	)
 
-	// ErrInvalidSplitAmount is an error returned when a set of splits does
-	// not fully consume the asset inputs.
-	ErrInvalidSplitAmount = errors.New(
-		"splits do not fully consume asset input amount",
-	)
+	// ErrInvalidSplitAmount is an error returned when a split amount is
+	// invalid (e.g. splits do not fully consume input amount).
+	ErrInvalidSplitAmount = errors.New("invalid split amounts")
 
 	// ErrInvalidSplitLocator is returned if a new split is attempted to be
 	// created w/o a valid external split locator.
