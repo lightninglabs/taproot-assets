89d44b40482f699e9c379da04f7851665bf3ff06
eugene <elzeigel@gmail.com>
2023-09-14 10:51:32 -0400

    proof: fuzz test for File and Proof

---
+++ proof/fuzz_test.go
@@ -0,0 +1,31 @@
+package proof
+
+import (
+	"bytes"
+	"testing"
+)
+
+func FuzzFile(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		fileData := make([]byte, 0)
+		fileData = append(fileData, FilePrefixMagicBytes[:]...)
+		fileData = append(fileData, data...)
+
+		f := &File{}
+
+		_ = f.Decode(bytes.NewReader(fileData))
+
+	})
+}
+
+func FuzzProof(f *testing.F) {
+	f.Fuzz(func(t *testing.T, data []byte) {
+		proof := &Proof{}
+
+		proofData := make([]byte, 0)
+		proofData = append(proofData, PrefixMagicBytes[:]...)
+		proofData = append(proofData, data...)
+
+		_ = proof.Decode(bytes.NewReader(proofData))
+	})
+}
