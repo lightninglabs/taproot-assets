14fd2b2a78458e8945559f71377987d4cac13f96
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2023-09-14 15:32:42 +0300

    tapscript+asset: virtual TXs for genesis assets
    In this commit, we define valid genesis assets when members of a group.
    We also define virtual TxIns and TxOuts for such assets.
    We combine these changes to support making VirtualTxs for
    genesis grouped assets with existing virtualTx constructors.

--- asset/mock.go
+++ asset/mock.go
@@ -2,12 +2,16 @@ package asset
 
 import (
 	"bytes"
+	"context"
+	"crypto/sha256"
+	"encoding/binary"
 	"encoding/hex"
 	"fmt"
 	"testing"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcec/v2/schnorr"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightninglabs/lndclient"
@@ -61,6 +65,160 @@ func RandGroupKeyWithSigner(t testing.TB, genesis Genesis) (*GroupKey, []byte) {
 
 	return groupKey, privateKey.Serialize()
 }
+
+// Copied from tapscript/tx.go.
+func virtualGenesisTxWithInput(virtualTx *wire.MsgTx, input *Asset,
+	idx uint32, witness wire.TxWitness) *wire.MsgTx {
+
+	txCopy := virtualTx.Copy()
+	txCopy.LockTime = uint32(input.LockTime)
+	// Using the standard zeroIndex for virtual prev outs.
+	txCopy.TxIn[0].PreviousOutPoint.Index = idx
+	txCopy.TxIn[0].Sequence = uint32(input.RelativeLockTime)
+	txCopy.TxIn[0].Witness = witness
+	return txCopy
+}
+
+// Copied from tapscript/tx.go.
+func inputGenesisAssetPrevOut(prevAsset Asset) (*wire.TxOut, error) {
+	switch prevAsset.ScriptVersion {
+	case ScriptV0:
+		// If the input asset is a genesis asset that is part of an
+		// asset group, we need to validate the group witness against
+		// the tweaked group key and not the genesis asset script key.
+		validationKey := &prevAsset.GroupKey.GroupPubKey
+		pkScript, err := test.ComputeTaprootScriptErr(
+			schnorr.SerializePubKey(validationKey),
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		return &wire.TxOut{
+			Value:    int64(prevAsset.Amount),
+			PkScript: pkScript,
+		}, nil
+	default:
+		return nil, ErrUnknownVersion
+	}
+}
+
+// Forked from tapscript/tx/virtualTxIn to remove checks for non-nil PrevIDs on
+// inputs and full consumption of inputs.
+func VirtualGenesisTxIn(newAsset *Asset) (*wire.TxIn, mssmt.Tree, error) {
+	inputTree := mssmt.NewCompactedTree(mssmt.NewDefaultStore())
+
+	// TODO(bhandras): thread the context through.
+	ctx := context.TODO()
+
+	key := ZeroPrevID.Hash()
+	leaf, err := newAsset.Leaf()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	_, err = inputTree.Insert(ctx, key, leaf)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	treeRoot, err := inputTree.Root(context.Background())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// Re-implement tapscript/tx/virtualTxInPrevOut directly here.
+	// TODO(roasbeef): document empty hash usage here
+	virtualTxInPrevOut := func(root mssmt.Node) *wire.OutPoint {
+		// Grab the hash digest of the SMT node. This'll be used to
+		// generate the virtual prev out for this tx in.
+		//
+		// TODO(roasbeef): this already contains the sum, so can just
+		// use it directly?
+		rootKey := root.NodeHash()
+
+		// Map this to: nodeHash || nodeSum.
+		h := sha256.New()
+		_, _ = h.Write(rootKey[:])
+		_ = binary.Write(h, binary.BigEndian, root.NodeSum())
+
+		// Using the standard zeroIndex for virtual prev outs.
+		return wire.NewOutPoint(
+			(*chainhash.Hash)(h.Sum(nil)), 0,
+		)
+	}
+	prevOut := virtualTxInPrevOut(treeRoot)
+
+	return wire.NewTxIn(prevOut, nil, nil), inputTree, nil
+}
+
+// Forked from tapscript/tx/virtualTxOut to remove checks for split commitments
+// and witness stripping.
+func virtualGenesisTxOut(newAsset *Asset) (*wire.TxOut, error) {
+	// Commit to the new asset directly. In this case, the output script is
+	// derived from the root of a MS-SMT containing the new asset.
+	groupKey := schnorr.SerializePubKey(&newAsset.GroupKey.GroupPubKey)
+	assetID := newAsset.Genesis.ID()
+
+	h := sha256.New()
+	_, _ = h.Write(groupKey)
+	_, _ = h.Write(assetID[:])
+	_, _ = h.Write(schnorr.SerializePubKey(newAsset.ScriptKey.PubKey))
+
+	key := *(*[32]byte)(h.Sum(nil))
+	leaf, err := newAsset.Leaf()
+	if err != nil {
+		return nil, err
+	}
+	outputTree := mssmt.NewCompactedTree(mssmt.NewDefaultStore())
+
+	// TODO(bhandras): thread the context through.
+	tree, err := outputTree.Insert(context.TODO(), key, leaf)
+	if err != nil {
+		return nil, err
+	}
+
+	treeRoot, err := tree.Root(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	rootKey := treeRoot.NodeHash()
+	pkScript, err := test.ComputeTaprootScriptErr(rootKey[:])
+	if err != nil {
+		return nil, err
+	}
+	return wire.NewTxOut(int64(newAsset.Amount), pkScript), nil
+}
+
+// Forked from tapscript/tx/virtualTx to be used only with the
+// RawGroupTxBuilder.
+func virtualGenesisTx(newAsset *Asset) (*wire.MsgTx, error) {
+	var (
+		txIn *wire.TxIn
+		err  error
+	)
+
+	// We'll start by mapping all inputs into a MS-SMT.
+	txIn, _, err = VirtualGenesisTxIn(newAsset)
+	if err != nil {
+		return nil, err
+	}
+
+	// Then we'll map all asset outputs into a single UTXO.
+	txOut, err := virtualGenesisTxOut(newAsset)
+	if err != nil {
+		return nil, err
+	}
+
+	// With our single input and output mapped, we're ready to construct our
+	// virtual transaction.
+	virtualTx := wire.NewMsgTx(2)
+	virtualTx.AddTxIn(txIn)
+	virtualTx.AddTxOut(txOut)
+	return virtualTx, nil
+}
+
 // SignOutputRaw creates a signature for a single input.
 // Taken from lnd/lnwallet/btcwallet/signer:L344, SignOutputRaw
 func SignOutputRaw(priv *btcec.PrivateKey, tx *wire.MsgTx,
--- tapscript/tx.go
+++ tapscript/tx.go
@@ -77,59 +77,54 @@ func virtualTxInPrevOut(root mssmt.Node) *wire.OutPoint {
 func virtualTxIn(newAsset *asset.Asset, prevAssets commitment.InputSet) (
 	*wire.TxIn, mssmt.Tree, error) {
 
-	// Genesis assets shouldn't have any inputs committed, so they'll have
-	// an empty input tree.
-	isGenesisAsset := newAsset.HasGenesisWitness()
 	inputTree := mssmt.NewCompactedTree(mssmt.NewDefaultStore())
-	if !isGenesisAsset {
-		// For each input we'll locate the asset UTXO beign spent, then
-		// insert that into a new SMT, with the key being the hash of
-		// the prevID pointer, and the value being the leaf itself.
-		inputsConsumed := make(
-			map[asset.PrevID]struct{}, len(prevAssets),
-		)
+	// For each input we'll locate the asset UTXO being spent, then
+	// insert that into a new SMT, with the key being the hash of
+	// the prevID pointer, and the value being the leaf itself.
+	inputsConsumed := make(
+		map[asset.PrevID]struct{}, len(prevAssets),
+	)
 
-		// TODO(bhandras): thread the context through.
-		ctx := context.TODO()
+	// TODO(bhandras): thread the context through.
+	ctx := context.TODO()
 
-		for _, input := range newAsset.PrevWitnesses {
-			// At this point, each input MUST have a prev ID.
-			if input.PrevID == nil {
-				return nil, nil, ErrNoInputs
-			}
-
-			// The set of prev assets are similar to the prev
-			// output fetcher used in taproot.
-			prevAsset, ok := prevAssets[*input.PrevID]
-			if !ok {
-				return nil, nil, ErrNoInputs
-			}
-
-			// Now we'll insert this prev asset leaf into the tree.
-			// The generated leaf includes the amount of the asset,
-			// so the sum of this tree will be the total amount
-			// being spent.
-			key := input.PrevID.Hash()
-			leaf, err := prevAsset.Leaf()
-			if err != nil {
-				return nil, nil, err
-			}
-			_, err = inputTree.Insert(ctx, key, leaf)
-			if err != nil {
-				return nil, nil, err
-			}
-
-			inputsConsumed[*input.PrevID] = struct{}{}
+	for _, input := range newAsset.PrevWitnesses {
+		// At this point, each input MUST have a prev ID.
+		if input.PrevID == nil {
+			return nil, nil, ErrNoInputs
 		}
 
-		// In this context, the set of referenced inputs should match
-		// the set of previous assets. This ensures no duplicate inputs
-		// are being spent.
-		//
-		// TODO(roasbeef): make further explicit?
-		if len(inputsConsumed) != len(prevAssets) {
-			return nil, nil, ErrInputMismatch
+		// The set of prev assets are similar to the prev
+		// output fetcher used in taproot.
+		prevAsset, ok := prevAssets[*input.PrevID]
+		if !ok {
+			return nil, nil, ErrNoInputs
 		}
+
+		// Now we'll insert this prev asset leaf into the tree.
+		// The generated leaf includes the amount of the asset,
+		// so the sum of this tree will be the total amount
+		// being spent.
+		key := input.PrevID.Hash()
+		leaf, err := prevAsset.Leaf()
+		if err != nil {
+			return nil, nil, err
+		}
+		_, err = inputTree.Insert(ctx, key, leaf)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		inputsConsumed[*input.PrevID] = struct{}{}
+	}
+
+	// In this context, the set of referenced inputs should match
+	// the set of previous assets. This ensures no duplicate inputs
+	// are being spent.
+	//
+	// TODO(roasbeef): make further explicit?
+	if len(inputsConsumed) != len(prevAssets) {
+		return nil, nil, ErrInputMismatch
 	}
 
 	treeRoot, err := inputTree.Root(context.Background())
@@ -220,8 +215,18 @@ func virtualTxOut(asset *asset.Asset) (*wire.TxOut, error) {
 func VirtualTx(newAsset *asset.Asset, prevAssets commitment.InputSet) (
 	*wire.MsgTx, mssmt.Tree, error) {
 
+	var (
+		txIn      *wire.TxIn
+		inputTree mssmt.Tree
+		err       error
+	)
+
 	// We'll start by mapping all inputs into a MS-SMT.
-	txIn, inputTree, err := virtualTxIn(newAsset, prevAssets)
+	if newAsset.NeedsGenesisWitnessForGroup() {
+		txIn, inputTree, err = asset.VirtualGenesisTxIn(newAsset)
+	} else {
+		txIn, inputTree, err = virtualTxIn(newAsset, prevAssets)
+	}
 	if err != nil {
 		return nil, nil, err
 	}
@@ -263,8 +268,19 @@ func InputAssetPrevOut(prevAsset asset.Asset) (*wire.TxOut, error) {
 	var pkScript []byte
 	switch prevAsset.ScriptVersion {
 	case asset.ScriptV0:
-		var err error
-		pkScript, err = PayToTaprootScript(prevAsset.ScriptKey.PubKey)
+		var (
+			err           error
+			validationKey = prevAsset.ScriptKey.PubKey
+		)
+
+		// If the input asset is a genesis asset that is part of an
+		// asset group, we need to validate the group witness against
+		// the tweaked group key and not the genesis asset script key.
+		if prevAsset.NeedsGenesisWitnessForGroup() {
+			validationKey = &prevAsset.GroupKey.GroupPubKey
+		}
+
+		pkScript, err = PayToTaprootScript(validationKey)
 		if err != nil {
 			return nil, err
 		}
