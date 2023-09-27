c3241aca2efdb70f2ce70fd3972b454feb8b0e09
Oliver Gugger <gugger@gmail.com>
2023-09-27 14:57:38 +0200

    commitment: remove unused encoder/decoder functions

--- commitment/encoding.go
+++ commitment/encoding.go
@@ -8,29 +8,6 @@ import (
 	"github.com/lightningnetwork/lnd/tlv"
 )
 
-func ProofEncoder(w io.Writer, val any, buf *[8]byte) error {
-	if t, ok := val.(*Proof); ok {
-		return t.Encode(w)
-	}
-	return tlv.NewTypeForEncodingErr(val, "*Proof")
-}
-
-func ProofDecoder(r io.Reader, val any, buf *[8]byte, l uint64) error {
-	if typ, ok := val.(*Proof); ok {
-		var proofBytes []byte
-		if err := tlv.DVarBytes(r, &proofBytes, buf, l); err != nil {
-			return err
-		}
-		var proof Proof
-		if err := proof.Decode(bytes.NewReader(proofBytes)); err != nil {
-			return err
-		}
-		*typ = proof
-		return nil
-	}
-	return tlv.NewTypeForEncodingErr(val, "*Proof")
-}
-
 func AssetProofEncoder(w io.Writer, val any, buf *[8]byte) error {
 	if t, ok := val.(**AssetProof); ok {
 		records := []tlv.Record{
