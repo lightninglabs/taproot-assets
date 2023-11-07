8ec03e30b45c7047628a60c968588ed20834417b
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-11-07 15:28:56 -0800

    tapdb: ensure all timers are fully stopped before restarting
    This fixes a bug where if the timer had already expired, then the
    receive on the timer channel would block.

--- tapdb/universe_stats.go
+++ tapdb/universe_stats.go
@@ -512,6 +512,7 @@ func (u *UniverseStats) populateSyncStatsCache() {
 
 	// If this is a test, then we'll just purge the items.
 	if u.opts.cacheDuration == 0 {
+		log.Debugf("nil state cache duration, wiping cache")
 		u.statsSnapshot.Store(nil)
 		return
 	}
@@ -535,7 +536,10 @@ func (u *UniverseStats) populateSyncStatsCache() {
 
 	// Reset the timer so we'll refresh again after the cache duration.
 	if !u.statsRefresh.Stop() {
-		<-u.statsRefresh.C
+		select {
+		case <-u.statsRefresh.C:
+		default:
+		}
 	}
 
 	u.statsRefresh.Reset(u.opts.cacheDuration)
@@ -565,6 +569,8 @@ func (u *UniverseStats) AggregateSyncStats(
 
 	u.statsCacheLogger.Miss()
 
+	log.Debugf("Populating aggregate sync stats")
+
 	dbStats, err := u.querySyncStats(ctx)
 	if err != nil {
 		return dbStats, err
@@ -574,6 +580,13 @@ func (u *UniverseStats) AggregateSyncStats(
 	// the stats pointer so we'll refresh it after a period of time.
 	u.statsSnapshot.Store(&dbStats)
 
+	// Reset the timer so we'll refresh again after the cache duration.
+	if u.statsRefresh != nil && !u.statsRefresh.Stop() {
+		select {
+		case <-u.statsRefresh.C:
+		default:
+		}
+	}
 	u.statsRefresh = time.AfterFunc(
 		u.opts.cacheDuration, u.populateSyncStatsCache,
 	)
@@ -844,7 +857,10 @@ func (u *UniverseStats) QuerySyncStats(ctx context.Context,
 	// If we already have a timer active, then stop it, so we only have a
 	// single timer going at any given time.
 	if u.syncStatsRefresh != nil && !u.syncStatsRefresh.Stop() {
-		<-u.syncStatsRefresh.C
+		select {
+		case <-u.syncStatsRefresh.C:
+		default:
+		}
 	}
 
 	u.syncStatsRefresh = time.AfterFunc(u.opts.cacheDuration, func() {
