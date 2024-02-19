596805dcc7ac14faa439deb7c92691000b761ac0
ffranr <robyn@lightning.engineering>
2024-02-19 17:24:53 +0000

    rfqmsg: add new RFQ message types package
    This commit adds a new package called rfqmsg which contains RFQ message
    types. Message types are: request, accept, and reject.

---
+++ rfqmsg/messages.go
@@ -0,0 +1,95 @@
+package rfqmsg
+
+import (
+	"encoding/hex"
+	"errors"
+	"math"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/routing/route"
+)
+
+// ID is the identifier for a RFQ message.
+type ID [32]byte
+
+// String returns the string representation of the ID.
+func (id ID) String() string {
+	return hex.EncodeToString(id[:])
+}
+
+// SerialisedScid is a serialised short channel id (SCID).
+type SerialisedScid uint64
+
+// MaxMessageType is the maximum supported message type value.
+const MaxMessageType = lnwire.MessageType(math.MaxUint16)
+
+// TapMessageTypeBaseOffset is the taproot-assets specific message type
+// identifier base offset. All tap messages will have a type identifier that is
+// greater than this value.
+//
+// This offset was chosen as the concatenation of the alphabetical index
+// positions of the letters "t" (20), "a" (1), and "p" (16).
+const TapMessageTypeBaseOffset = 20116 + lnwire.CustomTypeStart
+
+const (
+	// MsgTypeRequest is the message type identifier for a quote request
+	// message.
+	MsgTypeRequest = TapMessageTypeBaseOffset + 0
+
+	// MsgTypeAccept is the message type identifier for a quote accept
+	// message.
+	MsgTypeAccept = TapMessageTypeBaseOffset + 1
+
+	// MsgTypeReject is the message type identifier for a quote
+	// reject message.
+	MsgTypeReject = TapMessageTypeBaseOffset + 2
+)
+
+var (
+	// ErrUnknownMessageType is an error that is returned when an unknown
+	// message type is encountered.
+	ErrUnknownMessageType = errors.New("unknown message type")
+)
+
+// WireMessage is a struct that represents a general wire message.
+type WireMessage struct {
+	// Peer is the origin/destination peer for this message.
+	Peer route.Vertex
+
+	// MsgType is the protocol message type number.
+	MsgType lnwire.MessageType
+
+	// Data is the data exchanged.
+	Data []byte
+}
+
+// NewIncomingMsgFromWire creates a new RFQ message from a wire message.
+func NewIncomingMsgFromWire(wireMsg WireMessage) (IncomingMsg, error) {
+	switch wireMsg.MsgType {
+	case MsgTypeRequest:
+		return NewRequestMsgFromWire(wireMsg)
+	case MsgTypeAccept:
+		return NewAcceptFromWireMsg(wireMsg)
+	case MsgTypeReject:
+		return NewQuoteRejectFromWireMsg(wireMsg)
+	default:
+		return nil, ErrUnknownMessageType
+	}
+}
+
+// IncomingMsg is an interface that represents an inbound wire message
+// that has been received from a peer.
+type IncomingMsg interface {
+	// String returns a human-readable string representation of the message.
+	String() string
+}
+
+// OutgoingMsg is an interface that represents an outbound wire message
+// that can be sent to a peer.
+type OutgoingMsg interface {
+	// String returns a human-readable string representation of the message.
+	String() string
+
+	// ToWire returns a wire message with a serialized data field.
+	ToWire() (WireMessage, error)
+}
---
+++ rfqmsg/request_test.go
@@ -0,0 +1,70 @@
+package rfqmsg
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/lightninglabs/taproot-assets/asset"
+	"github.com/lightninglabs/taproot-assets/internal/test"
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestRequestMsgDataEncodeDecode tests the encoding and decoding of a request
+// message.
+func TestRequestMsgDataEncodeDecode(t *testing.T) {
+	t.Parallel()
+
+	// Create a random ID.
+	randomIdBytes := test.RandBytes(32)
+	id := ID(randomIdBytes)
+
+	// Create a random asset ID.
+	randomAssetIdBytes := test.RandBytes(32)
+	assetId := asset.ID(randomAssetIdBytes)
+
+	testCases := []struct {
+		testName string
+
+		id            ID
+		assetId       *asset.ID
+		assetGroupKey *btcec.PublicKey
+		assetAmount   uint64
+		bidPrice      lnwire.MilliSatoshi
+	}{
+		{
+			testName:      "asset group key nil",
+			id:            id,
+			assetId:       &assetId,
+			assetGroupKey: nil,
+			assetAmount:   1000,
+			bidPrice:      lnwire.MilliSatoshi(42000),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(tt *testing.T) {
+			req := requestMsgData{
+				ID:            tc.id,
+				AssetID:       tc.assetId,
+				AssetGroupKey: tc.assetGroupKey,
+				AssetAmount:   tc.assetAmount,
+				BidPrice:      tc.bidPrice,
+			}
+
+			// Encode the request message.
+			reqBytes, err := req.Bytes()
+			require.NoError(tt, err, "unable to encode request")
+
+			// Decode the request message.
+			decodedReq := requestMsgData{}
+			err = decodedReq.Decode(bytes.NewReader(reqBytes))
+			require.NoError(tt, err, "unable to decode request")
+
+			// Assert that the decoded request message is equal to
+			// the original request message.
+			require.Equal(tt, req, decodedReq)
+		})
+	}
+}
