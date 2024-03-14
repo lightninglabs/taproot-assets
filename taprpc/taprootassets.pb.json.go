26bf430cc6e50ba761bda47e0a1a3148e63ee02b
Oliver Gugger <gugger@gmail.com>
2024-03-14 19:16:05 +0100

    rpcserver+taprpc: add new SubscribeReceiveEvents RPC
    With this commit we add a new RPC that allows users to subscribe to
    address receive events in general or filtered by a specific address.
    This RPC notifies more granularly about the different states an inbound
    asset goes through, but hides some of the implementation details the
    previous (now development only) RPC showed (namely the proof courier
    backoff events).

--- taprpc/taprootassets.pb.json.go
+++ taprpc/taprootassets.pb.json.go
@@ -470,4 +470,46 @@ func RegisterTaprootAssetsJSONCallbacks(registry map[string]func(ctx context.Con
 		}
 		callback(string(respBytes), nil)
 	}
+
+	registry["taprpc.TaprootAssets.SubscribeReceiveEvents"] = func(ctx context.Context,
+		conn *grpc.ClientConn, reqJSON string, callback func(string, error)) {
+
+		req := &SubscribeReceiveEventsRequest{}
+		err := marshaler.Unmarshal([]byte(reqJSON), req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		client := NewTaprootAssetsClient(conn)
+		stream, err := client.SubscribeReceiveEvents(ctx, req)
+		if err != nil {
+			callback("", err)
+			return
+		}
+
+		go func() {
+			for {
+				select {
+				case <-stream.Context().Done():
+					callback("", stream.Context().Err())
+					return
+				default:
+				}
+
+				resp, err := stream.Recv()
+				if err != nil {
+					callback("", err)
+					return
+				}
+
+				respBytes, err := marshaler.Marshal(resp)
+				if err != nil {
+					callback("", err)
+					return
+				}
+				callback(string(respBytes), nil)
+			}
+		}()
+	}
 }
