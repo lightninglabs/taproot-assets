9846c04870d1fde0ad0e8d89332c33682c348a84
Oliver Gugger <gugger@gmail.com>
2024-04-15 21:59:20 +0200

    tappsbt: add FromProofs, rename file

--- tappsbt/create.go
+++ tappsbt/create.go
@@ -3,8 +3,11 @@ package tappsbt
 import (
 	"fmt"
 
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/lightninglabs/taproot-assets/address"
 	"github.com/lightninglabs/taproot-assets/asset"
+	"github.com/lightninglabs/taproot-assets/commitment"
 	"github.com/lightninglabs/taproot-assets/proof"
 	"github.com/lightningnetwork/lnd/keychain"
 )
@@ -143,4 +146,64 @@ func OwnershipProofPacket(ownedAsset *asset.Asset,
 	vPkt.SetInputAsset(0, ownedAsset)
 
 	return vPkt
+}
+
+// FromProofs creates a packet from the given proofs that adds them as inputs to
+// the packet.
+func FromProofs(proofs []*proof.Proof,
+	params *address.ChainParams) (*VPacket, error) {
+
+	pkt := &VPacket{
+		ChainParams: params,
+		Version:     0,
+	}
+
+	for idx := range proofs {
+		p := proofs[idx]
+
+		txOut := p.AnchorTx.TxOut[p.InclusionProof.OutputIndex]
+
+		//nolint:lll
+		_, tapCommitment, err := p.InclusionProof.DeriveByAssetInclusion(
+			&p.Asset,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error deriving commitment: %w",
+				err)
+		}
+
+		tapProof := p.InclusionProof.CommitmentProof
+
+		//nolint:lll
+		siblingBytes, sibling, err := commitment.MaybeEncodeTapscriptPreimage(
+			tapProof.TapSiblingPreimage,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("error encoding taproot "+
+				"sibling: %w", err)
+		}
+
+		rootHash := tapCommitment.TapscriptRoot(sibling)
+		pkt.Inputs = append(pkt.Inputs, &VInput{
+			PrevID: asset.PrevID{
+				OutPoint: p.OutPoint(),
+				ID:       p.Asset.ID(),
+				ScriptKey: asset.ToSerialized(
+					p.Asset.ScriptKey.PubKey,
+				),
+			},
+			Anchor: Anchor{
+				Value:            btcutil.Amount(txOut.Value),
+				PkScript:         txOut.PkScript,
+				SigHashType:      txscript.SigHashDefault,
+				InternalKey:      p.InclusionProof.InternalKey,
+				MerkleRoot:       rootHash[:],
+				TapscriptSibling: siblingBytes,
+			},
+			Proof: p,
+		})
+		pkt.SetInputAsset(len(pkt.Inputs)-1, &p.Asset)
+	}
+
+	return pkt, nil
 }
