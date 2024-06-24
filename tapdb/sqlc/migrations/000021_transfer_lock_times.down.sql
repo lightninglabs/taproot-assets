4bc339604bb3af324c7276b191c65e7982fe8cb5
Oliver Gugger <gugger@gmail.com>
2024-06-24 16:58:12 +0200

    sqlc/migrations: add lock times to transfer outputs

---
+++ tapdb/sqlc/migrations/000021_transfer_lock_times.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE asset_transfer_outputs DROP COLUMN lock_time;
+ALTER TABLE asset_transfer_outputs DROP COLUMN relative_lock_time;
---
+++ tapdb/sqlc/migrations/000021_transfer_lock_times.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE asset_transfer_outputs ADD COLUMN lock_time INTEGER;
+ALTER TABLE asset_transfer_outputs ADD COLUMN relative_lock_time INTEGER;
