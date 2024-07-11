168700c753fa9a796f246fd85fe3c025197044f2
Oliver Gugger <gugger@gmail.com>
2024-07-11 10:24:46 +0200

    rfq: only shut down on critical error
    Fixes #1010.
    
    With this commit we introduce a new fn.CriticalError type and only send
    RFQ errors to the main error channel (which causes the daemon to shut
    down) if an error is critical to the operation and can't just be logged.

--- fn/errors.go
+++ fn/errors.go
@@ -2,6 +2,7 @@ package fn
 
 import (
 	"context"
+	"errors"
 	"strings"
 
 	"google.golang.org/grpc/codes"
@@ -43,4 +44,43 @@ func IsRpcErr(err error, candidate error) bool {
 	}
 
 	return strings.Contains(err.Error(), candidate.Error())
+}
+
+// CriticalError is an error type that should be used for errors that are
+// critical and should cause the application to exit.
+type CriticalError struct {
+	Err error
+}
+
+// NewCriticalError creates a new CriticalError instance.
+func NewCriticalError(err error) *CriticalError {
+	return &CriticalError{Err: err}
+}
+
+// Error implements the error interface.
+func (e *CriticalError) Error() string {
+	return e.Err.Error()
+}
+
+// Unwrap implements the errors.Wrapper interface.
+func (e *CriticalError) Unwrap() error {
+	return e.Err
+}
+
+// ErrorAs behaves the same as `errors.As` except there's no need to declare
+// the target error as a variable first.
+// Instead of writing:
+//
+//	var targetErr *TargetErr
+//	errors.As(err, &targetErr)
+//
+// We can write:
+//
+//	lnutils.ErrorAs[*TargetErr](err)
+//
+// To save us from declaring the target error variable.
+func ErrorAs[Target error](err error) bool {
+	var targetErr Target
+
+	return errors.As(err, &targetErr)
 }
