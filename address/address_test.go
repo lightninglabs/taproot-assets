5a3a519da26948caf3f21abcb55e23ecefa453b8
Oliver Gugger <gugger@gmail.com>
2024-07-19 13:42:46 +0200

    address: make forward-compatible by parsing unknown odd types

--- address/address_test.go
+++ address/address_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/lightninglabs/taproot-assets/commitment"
 	"github.com/lightninglabs/taproot-assets/fn"
 	"github.com/lightninglabs/taproot-assets/internal/test"
+	"github.com/lightningnetwork/lnd/tlv"
 	"github.com/stretchr/testify/require"
 )
 
@@ -553,4 +554,58 @@ func FuzzAddressDecode(f *testing.F) {
 		a := &Tap{}
 		_ = a.Decode(bytes.NewReader(data))
 	})
+}
+
+// TestAddressUnknownOddType tests that an unknown odd type is allowed in an
+// address and that we can still arrive at the correct leaf hash with it.
+func TestAddressUnknownOddType(t *testing.T) {
+	knownAddr, _, _ := RandAddr(t, &TestNet3Tap, RandProofCourierAddr(t))
+	knownAddrString, err := knownAddr.EncodeAddress()
+	require.NoError(t, err)
+
+	var buf bytes.Buffer
+	err = knownAddr.Encode(&buf)
+	require.NoError(t, err)
+
+	// With the known address now encoded, we can add an unknown even type
+	// to the encoded bytes. That should provoke an error when parsed again.
+	unknownTypeValue := []byte("I could be anything, really")
+	unknownEvenType := append([]byte{
+		byte(40),                    // Type 40 is unknown.
+		byte(len(unknownTypeValue)), // Length of the value.
+	}, unknownTypeValue...)
+	buf.Write(unknownEvenType)
+
+	// Try to parse it again.
+	parsedAddr := &Tap{
+		ChainParams: &TestNet3Tap,
+	}
+	err = parsedAddr.Decode(&buf)
+	require.ErrorAs(t, err, &asset.ErrUnknownType{})
+
+	// Now clear the buffer, encode the address again, but this time add an
+	// unknown _odd_ type, which should be allowed.
+	unknownOddType := append([]byte{
+		byte(39),                    // Type 39 is unknown.
+		byte(len(unknownTypeValue)), // Length of the value.
+	}, unknownTypeValue...)
+	buf.Reset()
+	err = knownAddr.Encode(&buf)
+	require.NoError(t, err)
+	buf.Write(unknownOddType)
+
+	err = parsedAddr.Decode(&buf)
+	require.NoError(t, err)
+
+	expectedUnknownTypes := tlv.TypeMap{
+		39: unknownTypeValue,
+	}
+	require.Equal(t, expectedUnknownTypes, parsedAddr.unknownOddTypes)
+
+	// The leaf should've changed, to make sure the unknown value was taken
+	// into account when creating the serialized leaf.
+	parsedAddrString, err := parsedAddr.EncodeAddress()
+	require.NoError(t, err)
+
+	require.NotEqual(t, knownAddrString, parsedAddrString)
 }
