cbb3aa6e23d5cddb01e066e1b276d6871085c75f
Oliver Gugger <gugger@gmail.com>
2024-08-15 16:42:44 +0200

    migrations: re-apply migration 14 as 23.
    Due to a git branch cherry-pick mishap, the migration number 14 wasn't
    included in a released version and therefore wasn't applied on some
    systems. Because the tables in that migration file were only being used
    with a recently released version (v0.4.0/v0.4.1), this wasn't noticed
    for a long time.
    We fix this by re-applying the same migration as number 23, slightly
    modified so that it's a no-op if the tables and entries already exist on
    a system.

--- tapdb/migrations.go
+++ tapdb/migrations.go
@@ -22,7 +22,7 @@ const (
 	// daemon.
 	//
 	// NOTE: This MUST be updated when a new migration is added.
-	LatestMigrationVersion = 22
+	LatestMigrationVersion = 23
 )
 
 // MigrationTarget is a functional option that can be passed to applyMigrations
