23dee0a2f82804785516f21cedf63c7587b70dc7
ffranr <robyn@lightning.engineering>
2024-09-30 16:17:58 +0100

    rfq+rfqmsg: replace SellRequest.AskPrice with SuggestedAssetRate
    This commit replaces the SellRequest.AskPrice field with
    SuggestedAssetRate, changing a `uint64` price to an asset-to-BTC rate
    represented as a fixed-point number.

--- rfq/negotiator.go
+++ rfq/negotiator.go
@@ -458,12 +458,11 @@ func (n *Negotiator) HandleOutgoingSellOrder(order SellOrder) {
 		// We calculate a proposed ask price for our peer's
 		// consideration. If a price oracle is not specified we will
 		// skip this step.
-		var askPrice lnwire.MilliSatoshi
+		var assetRate fn.Option[rfqmath.BigIntFixedPoint]
 
 		if n.cfg.PriceOracle != nil {
 			// Query the price oracle for an asking price.
-			var err error
-			assetRate, _, err := n.queryAskFromPriceOracle(
+			rate, _, err := n.queryAskFromPriceOracle(
 				order.Peer, order.AssetID, order.AssetGroupKey,
 				order.MaxAssetAmount,
 				fn.None[rfqmath.BigIntFixedPoint](),
@@ -475,17 +474,12 @@ func (n *Negotiator) HandleOutgoingSellOrder(order SellOrder) {
 				return
 			}
 
-			// TODO(ffranr): This is a temporary solution which will
-			//  be re-written once RFQ quote request messages are
-			//  updated to include a suggested asset rate.
-			askPrice = lnwire.MilliSatoshi(
-				assetRate.Coefficient.ToUint64(),
-			)
+			assetRate = fn.Some[rfqmath.BigIntFixedPoint](*rate)
 		}
 
 		request, err := rfqmsg.NewSellRequest(
 			*order.Peer, order.AssetID, order.AssetGroupKey,
-			order.MaxAssetAmount, askPrice,
+			order.MaxAssetAmount, assetRate,
 		)
 		if err != nil {
 			err := fmt.Errorf("unable to create sell request "+
