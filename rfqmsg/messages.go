a94f33172acf401cabd598dd7d3a141e8cfb76b0
ffranr <robyn@lightning.engineering>
2024-10-04 18:03:11 +0100

    rfqmsg: add TlvFixedPoint
    This will be used for serializing fixed-points in wire messages.

--- rfqmsg/messages.go
+++ rfqmsg/messages.go
@@ -216,3 +216,54 @@ func NewBigIntFixedPoint(coefficient uint64, scale uint8) BigIntFixedPoint {
 		Scale:       scale,
 	}
 }
+
+// TlvFixedPoint is a fixed-point that can be TLV encode/decode.
+type TlvFixedPoint struct {
+	// fp is the underlying BigInt fixed-point.
+	fp BigIntFixedPoint
+}
+
+// NewTlvFixedPointFromBigInt creates a new fixed-point given a BigInt
+// fixed-point.
+func NewTlvFixedPointFromBigInt(fp BigIntFixedPoint) TlvFixedPoint {
+	return TlvFixedPoint{
+		fp: fp,
+	}
+}
+
+// NewTlvFixedPointFromUint64 creates a new fixed point record given a `uint64`
+// coefficient and scale.
+func NewTlvFixedPointFromUint64(coefficient uint64, scale uint8) TlvFixedPoint {
+	return TlvFixedPoint{
+		fp: NewBigIntFixedPoint(coefficient, scale),
+	}
+}
+
+// Record returns a TLV record that can be used to encode/decode an ID to/from a
+// TLV stream.
+//
+// NOTE: This is part of the tlv.RecordProducer interface.
+func (f *TlvFixedPoint) Record() tlv.Record {
+	recordSize := func() uint64 {
+		// 1 byte for the scale (uint8)
+		scaleLength := uint64(1)
+
+		coefficientBytesLength := uint64(len(f.fp.Coefficient.Bytes()))
+		return scaleLength + coefficientBytesLength
+	}
+
+	// Note that the type here is set to zero, as when used with a
+	// tlv.RecordT, the type param will be used as the type.
+	return tlv.MakeDynamicRecord(
+		0, f, recordSize, TlvFixedPointEncoder,
+		TlvFixedPointDecoder,
+	)
+}
+
+// IntoBigIntFixedPoint converts the TlvFixedPoint to a BigIntFixedPoint.
+func (f *TlvFixedPoint) IntoBigIntFixedPoint() BigIntFixedPoint {
+	return BigIntFixedPoint{
+		Coefficient: f.fp.Coefficient,
+		Scale:       f.fp.Scale,
+	}
+}
---
+++ rfqmsg/messages_test.go
@@ -0,0 +1,96 @@
+package rfqmsg
+
+import (
+	"bytes"
+	"math"
+	"math/big"
+	"testing"
+
+	"github.com/lightninglabs/taproot-assets/rfqmath"
+	"github.com/lightningnetwork/lnd/tlv"
+	"github.com/stretchr/testify/require"
+)
+
+// TestTlvFixedPoint tests encoding and decoding of the TlvFixedPoint struct.
+func TestTlvFixedPoint(t *testing.T) {
+	// This is the test case structure which will be encoded and decoded.
+	type testStruct struct {
+		ExchangeRate tlv.RecordT[tlv.TlvType1, TlvFixedPoint]
+	}
+
+	type testCase struct {
+		// name is the name of the test case.
+		name string
+
+		// tStruct is the test structure to encode and decode.
+		tStruct testStruct
+	}
+
+	// Create a large coefficient to test encoding and decoding of a large
+	// fixed-point value. Ensure it's larger than the maximum uint64 value.
+	bigInt := new(big.Int)
+	bigInt.SetUint64(math.MaxUint64)
+	bigInt = bigInt.Mul(bigInt, bigInt)
+
+	largeCFixedPoint := BigIntFixedPoint{
+		Coefficient: rfqmath.NewBigInt(bigInt),
+		Scale:       math.MaxUint8,
+	}
+
+	// Define test cases.
+	//
+	// nolint: lll
+	testCases := []testCase{
+		{
+			name: "coefficient as uint64",
+			tStruct: testStruct{
+				ExchangeRate: tlv.NewRecordT[tlv.TlvType1](
+					NewTlvFixedPointFromUint64(123456789, 7),
+				),
+			},
+		},
+		{
+			name: "coefficient as large BigInt",
+			tStruct: testStruct{
+				ExchangeRate: tlv.NewRecordT[tlv.TlvType1](
+					NewTlvFixedPointFromBigInt(largeCFixedPoint),
+				),
+			},
+		},
+	}
+
+	for testCaseIdx := range testCases {
+		tc := testCases[testCaseIdx]
+		tStruct := tc.tStruct
+
+		// Encode the test case.
+		tlvStream, err := tlv.NewStream(tStruct.ExchangeRate.Record())
+		require.NoError(t, err)
+
+		var buf bytes.Buffer
+		err = tlvStream.Encode(&buf)
+		require.NoError(t, err)
+
+		// Decode the test case.
+		decodedTc := &testStruct{}
+		tlvStream, err = tlv.NewStream(decodedTc.ExchangeRate.Record())
+		require.NoError(t, err)
+
+		err = tlvStream.Decode(&buf)
+		require.NoError(t, err)
+
+		// Compare the original and decoded test cases.
+		//
+		// Ensure scale is the same.
+		require.Equal(
+			t, tStruct.ExchangeRate.Val.fp.Scale,
+			decodedTc.ExchangeRate.Val.fp.Scale,
+		)
+
+		// Ensure coefficient is the same.
+		isEqual := tStruct.ExchangeRate.Val.fp.Coefficient.Equals(
+			decodedTc.ExchangeRate.Val.fp.Coefficient,
+		)
+		require.True(t, isEqual)
+	}
+}
