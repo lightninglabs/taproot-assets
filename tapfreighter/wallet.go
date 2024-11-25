19745285cf8a52ee20d91bceaa7479bb8a4aa475
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2024-11-25 15:38:37 -0500

    tapfreighter: trim altLeaves from passive vPkts
    In this commit, we update the passive asset handling to not consider
    altLeaves as assets. Specifically, any altLeaf in an input Tap
    commitment should be set in the vInput for a passive asset vPkt, but
    should not have its own vPkt where it is an input. Overall, input
    altLeaves do not need to be re-anchored.

--- tapfreighter/wallet.go
+++ tapfreighter/wallet.go
@@ -271,7 +271,8 @@ func (f *AssetWallet) FundAddressSend(ctx context.Context,
 func createPassivePacket(params *address.ChainParams, passiveAsset *asset.Asset,
 	activePackets []*tappsbt.VPacket, anchorOutputIndex uint32,
 	anchorOutputInternalKey keychain.KeyDescriptor, prevOut wire.OutPoint,
-	inputProof *proof.Proof) (*tappsbt.VPacket, error) {
+	inputProof *proof.Proof,
+	inputAltLeaves []*asset.Asset) (*tappsbt.VPacket, error) {
 
 	// Specify virtual input.
 	inputAsset := passiveAsset.Copy()
@@ -281,8 +282,12 @@ func createPassivePacket(params *address.ChainParams, passiveAsset *asset.Asset,
 			SighashType: txscript.SigHashDefault,
 		},
 	}
+	err := vInput.SetAltLeaves(inputAltLeaves)
+	if err != nil {
+		return nil, err
+	}
 
-	err := tapsend.ValidateVPacketVersions(activePackets)
+	err = tapsend.ValidateVPacketVersions(activePackets)
 	if err != nil {
 		return nil, err
 	}
@@ -580,7 +585,14 @@ func (f *AssetWallet) hasOtherAssets(inputCommitments tappsbt.InputCommitments,
 			return false, err
 		}
 
-		if len(passiveCommitments.CommittedAssets()) > 0 {
+		trimmedPassiveCommitments, _, err := commitment.TrimAltLeaves(
+			passiveCommitments,
+		)
+		if err != nil {
+			return false, err
+		}
+
+		if len(trimmedPassiveCommitments.CommittedAssets()) > 0 {
 			return true, nil
 		}
 	}
@@ -1243,7 +1255,14 @@ func (f *AssetWallet) CreatePassiveAssets(ctx context.Context,
 			return nil, err
 		}
 
-		passiveAssets := passiveCommitments.CommittedAssets()
+		trimmedPassives, altLeaves, err := commitment.TrimAltLeaves(
+			passiveCommitments,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		passiveAssets := trimmedPassives.CommittedAssets()
 		if len(passiveAssets) == 0 {
 			continue
 		}
@@ -1272,7 +1291,7 @@ func (f *AssetWallet) CreatePassiveAssets(ctx context.Context,
 			passivePacket, err := createPassivePacket(
 				f.cfg.ChainParams, passiveAsset, activePackets,
 				anchorOutIdx, *anchorOutDesc, prevID.OutPoint,
-				inputProof,
+				inputProof, altLeaves,
 			)
 			if err != nil {
 				return nil, fmt.Errorf("unable to create "+
