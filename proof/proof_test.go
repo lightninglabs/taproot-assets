17f4e95e80fc4e78c0739a8cb0af736191625855
Jonathan Harvey-Buschel <j.s.harbus@gmail.com>
2024-12-12 10:28:36 +0100

    proof: tappsbt: update BIP test vectors

--- proof/proof_test.go
+++ proof/proof_test.go
@@ -287,14 +287,11 @@ func genRandomGenesisWithProof(t testing.TB, assetType asset.Type,
 	// Include 1 or more alt leaves in the anchor output Tap commitment.
 	// Since this is also used for generating the test vectors, we don't
 	// actually want to have zero alt leaves.
-	innerAltLeaves := asset.RandAltLeaves(t, true)
-
-	altCommitment, err := commitment.NewAssetCommitment(innerAltLeaves...)
-	require.NoError(t, err)
-
-	err = tapCommitment.Upsert(altCommitment)
-	require.NoError(t, err)
+	innerAltLeaves := asset.RandAltLeaves(t, false)
 	altLeaves := asset.ToAltLeaves(innerAltLeaves)
+	err = tapCommitment.MergeAltLeaves(altLeaves)
+	require.NoError(t, err)
+
 	genesisAsset := assets[0]
 	_, commitmentProof, err := tapCommitment.Proof(
 		genesisAsset.TapCommitmentKey(),
