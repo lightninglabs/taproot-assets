2c0e65148eeba4f894a3dd36b909f4a016ac8c68
Oliver Gugger <gugger@gmail.com>
2024-12-19 13:44:03 +0100

    version: bump version to v0.5.0-alpha

--- version.go
+++ version.go
@@ -52,7 +52,7 @@ const (
 
 	// AppPreRelease defines the pre-release version of this binary.
 	// It MUST only contain characters from the semantic versioning spec.
-	AppPreRelease = "rc2"
+	AppPreRelease = ""
 
 	// GitTagIncludeStatus indicates whether the status should be included
 	// in the git tag name.
