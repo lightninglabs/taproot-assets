94a1a07e8ce4057fac5c38aca2ed8e61e62e592b
ffranr <ffranr@proton.me>
2025-01-02 13:33:33 +0000

    tapchannel: use trace log level for unhandled channel update messages
    Change the log level from debug to trace for unhandled channel update
    messages. This reduces noise in the debug logs while still allowing
    visibility when needed.

--- tapchannel/aux_funding_controller.go
+++ tapchannel/aux_funding_controller.go
@@ -2136,8 +2136,8 @@ func (f *FundingController) CanHandle(msg msgmux.PeerMsg) bool {
 		return true
 	}
 
-	log.Debugf("Failed to handle: %T", msg.Message)
-
+	log.Tracef("FundingController encountered an unsupported message "+
+		"type: %T", msg.Message)
 	return false
 }
 
