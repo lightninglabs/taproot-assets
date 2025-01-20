a64c36c92bfea5398ac728cb1a3165128e120c37
ffranr <ffranr@proton.me>
2025-01-20 12:33:12 +0000

    tapcli: move app construction to `commands` file
    Export a new function `NewApp` to handle app construction.
    
    This change simplifies the transition when `commands.go` is moved
    to a new package by reducing the number of exported symbols,
    resulting in a cleaner refactor.

--- cmd/tapcli/commands.go
+++ cmd/tapcli/commands.go
@@ -7,11 +7,101 @@ import (
 	"fmt"
 	"os"
 
+	tap "github.com/lightninglabs/taproot-assets"
 	"github.com/lightninglabs/taproot-assets/taprpc"
 	"github.com/lightningnetwork/lnd/signal"
 	"github.com/urfave/cli"
 	"google.golang.org/protobuf/proto"
 )
+
+// NewApp creates a new tapcli app with all the available commands.
+func NewApp() cli.App {
+	app := cli.NewApp()
+	app.Name = "tapcli"
+	app.Version = tap.Version()
+	app.Usage = "control plane for your Taproot Assets Daemon (tapd)"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "rpcserver",
+			Value: defaultRPCHostPort,
+			Usage: "The host:port of tap daemon.",
+		},
+		cli.StringFlag{
+			Name:      "tapddir",
+			Value:     defaultTapdDir,
+			Usage:     "The path to tap's base directory.",
+			TakesFile: true,
+		},
+		cli.StringFlag{
+			Name: "socksproxy",
+			Usage: "The host:port of a SOCKS proxy through " +
+				"which all connections to the Taproot Asset " +
+				"daemon will be established over.",
+		},
+		cli.StringFlag{
+			Name:      "tlscertpath",
+			Value:     defaultTLSCertPath,
+			Usage:     "The path to tapd's TLS certificate.",
+			TakesFile: true,
+		},
+		cli.StringFlag{
+			Name: "network, n",
+			Usage: "The network tapd is running on, e.g. " +
+				"mainnet, testnet, etc.",
+			Value: "testnet",
+		},
+		cli.BoolFlag{
+			Name:  "no-macaroons",
+			Usage: "Disable macaroon authentication.",
+		},
+		cli.StringFlag{
+			Name:      "macaroonpath",
+			Usage:     "The path to macaroon file.",
+			TakesFile: true,
+		},
+		cli.Int64Flag{
+			Name:  "macaroontimeout",
+			Value: 60,
+			Usage: "Anti-replay macaroon validity time in seconds.",
+		},
+		cli.StringFlag{
+			Name:  "macaroonip",
+			Usage: "If set, lock macaroon to specific IP address.",
+		},
+		cli.StringFlag{
+			Name: "profile, p",
+			Usage: "Instead of reading settings from command " +
+				"line parameters or using the default " +
+				"profile, use a specific profile. If " +
+				"a default profile is set, this flag can be " +
+				"set to an empty string to disable reading " +
+				"values from the profiles file.",
+		},
+		cli.StringFlag{
+			Name: "macfromjar",
+			Usage: "Use this macaroon from the profile's " +
+				"macaroon jar instead of the default one. " +
+				"Can only be used if profiles are defined.",
+		},
+	}
+
+	// Add all the available commands.
+	app.Commands = []cli.Command{
+		stopCommand,
+		debugLevelCommand,
+		profileSubCommand,
+		getInfoCommand,
+	}
+	app.Commands = append(app.Commands, assetsCommands...)
+	app.Commands = append(app.Commands, addrCommands...)
+	app.Commands = append(app.Commands, eventCommands...)
+	app.Commands = append(app.Commands, proofCommands...)
+	app.Commands = append(app.Commands, rfqCommands...)
+	app.Commands = append(app.Commands, universeCommands...)
+	app.Commands = append(app.Commands, devCommands...)
+
+	return *app
+}
 
 func getContext() context.Context {
 	shutdownInterceptor, err := signal.Intercept()
