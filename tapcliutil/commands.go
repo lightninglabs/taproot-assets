3ba7ba1af8b3fb247a228bc4449c72a957c65b18
ffranr <ffranr@proton.me>
2025-01-20 17:07:58 +0000

    tapcliutil: wrap command actions to target a specific RPC client
    Introduce functionality to wrap command actions, enabling them
    to target a specific RPC client (e.g., the `itest` tapd harness)
    directly. This eliminates the need to parse command-line arguments
    to determine the target tapd instance.

--- tapcliutil/commands.go
+++ tapcliutil/commands.go
@@ -9,6 +9,12 @@ import (
 
 	tap "github.com/lightninglabs/taproot-assets"
 	"github.com/lightninglabs/taproot-assets/taprpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/assetwalletrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/mintrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/rfqrpc"
+	tchrpc "github.com/lightninglabs/taproot-assets/taprpc/tapchannelrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/tapdevrpc"
+	"github.com/lightninglabs/taproot-assets/taprpc/universerpc"
 	"github.com/lightningnetwork/lnd/signal"
 	"github.com/urfave/cli"
 	"google.golang.org/protobuf/proto"
@@ -101,6 +107,18 @@ func NewApp() cli.App {
 	app.Commands = append(app.Commands, devCommands...)
 
 	return *app
+}
+
+// ClientInterfaceBundle is an interface that bundles all the client interfaces
+// that are used by the tapcli.
+type ClientInterfaceBundle interface {
+	taprpc.TaprootAssetsClient
+	assetwalletrpc.AssetWalletClient
+	mintrpc.MintClient
+	rfqrpc.RfqClient
+	tchrpc.TaprootAssetChannelsClient
+	universerpc.UniverseClient
+	tapdevrpc.TapDevClient
 }
 
 func getContext() context.Context {
@@ -221,4 +239,104 @@ func getInfo(ctx *cli.Context) error {
 
 	printRespJSON(resp)
 	return nil
+}
+
+// RpcClientHarness is a struct that contains the necessary information to
+// manage a client connection and the response and error channels for the
+// client.
+type RpcClientHarness struct {
+	// Ctx is the context for the client connection.
+	Ctx context.Context
+
+	// Client is the client RPC connection.
+	Client ClientInterfaceBundle
+
+	// ResponseChan is the channel to send the response to.
+	ResponseChan chan<- interface{}
+
+	// ErrChan is the channel to send the error to.
+	ErrChan chan<- error
+}
+
+// NewRpcClientHarness creates a new RpcClientHarness.
+func NewRpcClientHarness(ctx context.Context, client ClientInterfaceBundle,
+	responseChan chan<- interface{},
+	errChan chan<- error) *RpcClientHarness {
+
+	return &RpcClientHarness{
+		Ctx:          ctx,
+		Client:       client,
+		ResponseChan: responseChan,
+		ErrChan:      errChan,
+	}
+}
+
+// UnwrappedAction is a function signatures for unwrapped actions that are
+// executed by the wrapped action.
+type UnwrappedAction func(cliCtx *cli.Context, ctx context.Context,
+	client taprpc.TaprootAssetsClient, silencePrint bool) (proto.Message,
+	error)
+
+// WrappedAction is a function signature for wrapped actions that are executed
+// by cli.
+type WrappedAction = func(*cli.Context) error
+
+// NewWrappedAction creates a new WrappedAction that wraps an UnwrappedAction.
+func NewWrappedAction(rpcClientHarness *RpcClientHarness,
+	action UnwrappedAction) WrappedAction {
+
+	return func(cliCtx *cli.Context) error {
+		var (
+			ctx           context.Context
+			client        taprpc.TaprootAssetsClient
+			clientCleanUp func()
+
+			// We silence print statements if a client harness is
+			// provided.
+			silencePrint = rpcClientHarness != nil
+		)
+
+		// If an RPC client harness is provided, use it to get the
+		// client and context. Otherwise, create a new RPC client
+		// connection and context using the provided CLI context.
+		switch rpcClientHarness {
+		case nil:
+			// No RPC client harness provided, create a new client
+			// using the CLI context.
+			ctx = getContext()
+
+			// TODO(ffranr): Client here should satisfy interface
+			//  ClientInterfaceBundle.
+			client, clientCleanUp = getClient(cliCtx)
+		default:
+			// Use the provided RPC client harness to get the client
+			// and context.
+			ctx = rpcClientHarness.Ctx
+			client = rpcClientHarness.Client
+			clientCleanUp = func() {}
+		}
+
+		// Defer client cleanup.
+		defer clientCleanUp()
+
+		// Execute underlying action using the RPC client.
+		resp, err := action(cliCtx, ctx, client, silencePrint)
+		if err != nil {
+			// If an RPC client harness is provided, send the error
+			// to the error channel.
+			if rpcClientHarness != nil {
+				rpcClientHarness.ErrChan <- err
+			}
+
+			return err
+		}
+
+		// If an RPC client harness is provided, send the response to
+		// the response channel.
+		if rpcClientHarness != nil {
+			rpcClientHarness.ResponseChan <- resp
+		}
+
+		return nil
+	}
 }
