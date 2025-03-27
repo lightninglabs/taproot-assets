19d8296ae2788e6801913b6fc006a912c550a9a4
Oliver Gugger <gugger@gmail.com>
2025-03-27 12:29:08 -0500

    tapchannel+tapscript: derive unique funding script keys
    To avoid a proof collision in the universe, we need to make sure we
    derive unique funding output script keys for multiple asset IDs within
    the same channel funding outpoint. We do that by adding a second
    OP_RETURN leaf into the tapscript tree of the funding script key. That
    should ensure uniqueness of the top-level Taproot output key and only
    requires us to slightly alter the control block when creating the
    witness (it will now include an inclusion proof element for the
    OP_RETURN leaf).

--- tapscript/script.go
+++ tapscript/script.go
@@ -1,7 +1,11 @@
 package tapscript
 
 import (
+	"fmt"
+
 	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/lightninglabs/taproot-assets/asset"
 	"github.com/lightningnetwork/lnd/input"
 )
 
@@ -47,4 +51,88 @@ func NewChannelFundingScriptTree() *FundingScriptTree {
 			TapscriptRoot: tapScriptRoot[:],
 		},
 	}
+}
+
+// NewChannelFundingScriptTreeUniqueID creates a new funding script tree for a
+// custom channel asset-level script key. The script tree is constructed with a
+// simple OP_TRUE script that allows anyone to spend the output and another
+// OP_RETURN script that makes the resulting script key unique. This simplifies
+// the funding process as no signatures for the asset-level witnesses need to be
+// exchanged. This is still safe because the BTC level multi-sig output is still
+// protected by a 2-of-2 MuSig2 output.
+func NewChannelFundingScriptTreeUniqueID(id asset.ID) (*FundingScriptTree,
+	error) {
+
+	// First, we'll generate our OP_TRUE script.
+	fundingScript := AnyoneCanSpendScript()
+	fundingTapLeaf := txscript.NewBaseTapLeaf(fundingScript)
+
+	// Then we'll create the OP_RETURN leaf with the asset ID to make the
+	// resulting script key unique.
+	opReturnLeaf, err := asset.NewNonSpendableScriptLeaf(
+		asset.OpReturnVersion, id[:],
+	)
+	if err != nil {
+		return nil, fmt.Errorf("error deriving op return leaf: %w", err)
+	}
+
+	// With the both scripts derived, we'll now create the tapscript tree
+	// from them.
+	tapscriptTree := txscript.AssembleTaprootScriptTree(
+		fundingTapLeaf, opReturnLeaf,
+	)
+	tapScriptRoot := tapscriptTree.RootNode.TapHash()
+
+	// Finally, we'll make the funding output script which actually uses a
+	// NUMS key to force a script path only.
+	fundingOutputKey := txscript.ComputeTaprootOutputKey(
+		&input.TaprootNUMSKey, tapScriptRoot[:],
+	)
+
+	return &FundingScriptTree{
+		ScriptTree: input.ScriptTree{
+			InternalKey:   &input.TaprootNUMSKey,
+			TaprootKey:    fundingOutputKey,
+			TapscriptTree: tapscriptTree,
+			TapscriptRoot: tapScriptRoot[:],
+		},
+	}, nil
+}
+
+// ChannelFundingSpendWitness creates a complete witness to spend the OP_TRUE
+// funding script of an asset funding output.
+func ChannelFundingSpendWitness(uniqueScriptKeys bool,
+	assetID asset.ID) (wire.TxWitness, error) {
+
+	fundingScriptTree := NewChannelFundingScriptTree()
+
+	// If we're using unique script keys for multiple virtual packets with
+	// different asset IDs, we need to derive a specific script tree that
+	// includes the asset ID. Everything else should still work the same
+	// way (the OP_TRUE leaf we spend is still at index zero).
+	if uniqueScriptKeys {
+		var err error
+		fundingScriptTree, err = NewChannelFundingScriptTreeUniqueID(
+			assetID,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("unable to create unique "+
+				"script key: %w", err)
+		}
+	}
+
+	const opTrueIndex = 0
+	tapscriptTree := fundingScriptTree.TapscriptTree
+	ctrlBlock := tapscriptTree.LeafMerkleProofs[opTrueIndex].ToControlBlock(
+		&input.TaprootNUMSKey,
+	)
+	ctrlBlockBytes, err := ctrlBlock.ToBytes()
+	if err != nil {
+		return nil, fmt.Errorf("unable to serialize control "+
+			"block: %w", err)
+	}
+
+	return wire.TxWitness{
+		AnyoneCanSpendScript(), ctrlBlockBytes,
+	}, nil
 }
