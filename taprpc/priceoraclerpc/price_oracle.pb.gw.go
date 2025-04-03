5da431566cfb6dd1d69eba2b9ecb70ccd78b3e4b
Oliver Gugger <gugger@gmail.com>
2025-04-03 14:51:38 +0200

    taprpc: re-compile after update of protobuf library
    The previous commit forced us to update some gRPC/REST related
    libraries, which cause a diff in the generated RPC stubs.

--- taprpc/priceoraclerpc/price_oracle.pb.gw.go
+++ taprpc/priceoraclerpc/price_oracle.pb.gw.go
@@ -104,21 +104,21 @@ func RegisterPriceOracleHandlerServer(ctx context.Context, mux *runtime.ServeMux
 // RegisterPriceOracleHandlerFromEndpoint is same as RegisterPriceOracleHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterPriceOracleHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.DialContext(ctx, endpoint, opts...)
+	conn, err := grpc.NewClient(endpoint, opts...)
 	if err != nil {
 		return err
 	}
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Errorf("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
