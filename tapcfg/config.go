852d93ad90e99f651b0511781d7556de4b01c7da
George Tsagkarelis <george.tsagkarelis@gmail.com>
2025-05-16 14:36:50 +0200

    multi: allow user defined psbt max fee ratio
    This commit allows the user to set their own value for the maximum
    allowed fees to total output amount ratio that is used by the wallet
    when funding PSBTs.

--- tapcfg/config.go
+++ tapcfg/config.go
@@ -22,6 +22,7 @@ import (
 	"github.com/lightninglabs/lndclient"
 	tap "github.com/lightninglabs/taproot-assets"
 	"github.com/lightninglabs/taproot-assets/fn"
+	"github.com/lightninglabs/taproot-assets/lndservices"
 	"github.com/lightninglabs/taproot-assets/monitoring"
 	"github.com/lightninglabs/taproot-assets/proof"
 	"github.com/lightninglabs/taproot-assets/rfq"
@@ -143,6 +144,10 @@ const (
 	// defaultMailboxAuthTimeout is the default timeout we'll use for
 	// mailbox message retrieval client authentication.
 	defaultMailboxAuthTimeout = 10 * time.Second
+
+	// DefaultPsbtMaxFeeRatio is the default maximum for fees to total
+	// output amount ratio to use when funding PSBTs.
+	DefaultPsbtMaxFeeRatio = lndservices.DefaultPsbtMaxFeeRatio
 )
 
 var (
@@ -280,6 +285,17 @@ type LndConfig struct {
 	RPCTimeout time.Duration `long:"rpctimeout" description:"The timeout to use for RPC requests to lnd; a sufficiently long duration should be chosen to avoid issues with slow responses. Valid time units are {s, m, h}."`
 }
 
+// WalletConfig is the config that contains wallet related configurations.
+type WalletConfig struct {
+	// PsbtMaxFeeRatio is the maximum fees to total output amount ratio to
+	// use when funding PSBTs for asset transfers. Since taproot assets can
+	// be anchored to outpoints that may carry relatively small bitcoin
+	// amounts it is useful to pick a high value for this argument as in
+	// high fee environments the total fees paid may outweight the anchor
+	// amount. The allowed values for this argument range from 0.00 to 1.00.
+	PsbtMaxFeeRatio float64 `long:"psbt-max-fee-ratio" description:"The maximum fees to total output amount ratio to use when funding PSBTs for asset transfers. Value must be between 0.00 and 1.00"`
+}
+
 // UniverseConfig is the config that houses any Universe related config
 // values.
 type UniverseConfig struct {
@@ -360,6 +376,8 @@ type Config struct {
 	Postgres        *tapdb.PostgresConfig `group:"postgres" namespace:"postgres"`
 
 	Universe *UniverseConfig `group:"universe" namespace:"universe"`
+
+	Wallet *WalletConfig `group:"wallet" namespace:"wallet"`
 
 	AddrBook *AddrBookConfig `group:"address" namespace:"address"`
 
@@ -469,6 +487,9 @@ func DefaultConfig() Config {
 			),
 			MboxAuthTimeout:       defaultMailboxAuthTimeout,
 			SupplyIgnoreCacheSize: tapdb.DefaultNegativeLookupCacheSize,
+		},
+		Wallet: &WalletConfig{
+			PsbtMaxFeeRatio: DefaultPsbtMaxFeeRatio,
 		},
 		AddrBook: &AddrBookConfig{
 			DisableSyncer: false,
