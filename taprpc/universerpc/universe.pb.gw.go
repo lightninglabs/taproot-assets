7cd94339682d381ec4fbac6cae683bfb9a24fcb8
ffranr <ffranr@proton.me>
2025-05-27 19:24:07 +0100

    taprpc: add endpoint IgnoreAssetOutPoint
    Introduce a new RPC endpoint to add an asset outpoint to the asset's
    ignore tree. This allows asset issuers to explicitly exclude certain
    asset outpoints.

--- taprpc/universerpc/universe.pb.gw.go
+++ taprpc/universerpc/universe.pb.gw.go
@@ -1585,6 +1585,32 @@ func local_request_Universe_QueryFederationSyncConfig_0(ctx context.Context, mar
 
 }
 
+func request_Universe_IgnoreAssetOutPoint_0(ctx context.Context, marshaler runtime.Marshaler, client UniverseClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IgnoreAssetOutPointRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.IgnoreAssetOutPoint(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_Universe_IgnoreAssetOutPoint_0(ctx context.Context, marshaler runtime.Marshaler, server UniverseServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq IgnoreAssetOutPointRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.IgnoreAssetOutPoint(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterUniverseHandlerServer registers the http handlers for service Universe to "mux".
 // UnaryRPC     :call UniverseServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -2216,6 +2242,31 @@ func RegisterUniverseHandlerServer(ctx context.Context, mux *runtime.ServeMux, s
 
 	})
 
+	mux.Handle("POST", pattern_Universe_IgnoreAssetOutPoint_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/universerpc.Universe/IgnoreAssetOutPoint", runtime.WithHTTPPathPattern("/v1/taproot-assets/universe/ignore-asset-outpoint"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_Universe_IgnoreAssetOutPoint_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Universe_IgnoreAssetOutPoint_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -2807,6 +2858,28 @@ func RegisterUniverseHandlerClient(ctx context.Context, mux *runtime.ServeMux, c
 
 	})
 
+	mux.Handle("POST", pattern_Universe_IgnoreAssetOutPoint_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/universerpc.Universe/IgnoreAssetOutPoint", runtime.WithHTTPPathPattern("/v1/taproot-assets/universe/ignore-asset-outpoint"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_Universe_IgnoreAssetOutPoint_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_Universe_IgnoreAssetOutPoint_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -2860,6 +2933,8 @@ var (
 	pattern_Universe_SetFederationSyncConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "taproot-assets", "universe", "sync", "config"}, ""))
 
 	pattern_Universe_QueryFederationSyncConfig_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 2, 4}, []string{"v1", "taproot-assets", "universe", "sync", "config"}, ""))
+
+	pattern_Universe_IgnoreAssetOutPoint_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"v1", "taproot-assets", "universe", "ignore-asset-outpoint"}, ""))
 )
 
 var (
@@ -2912,4 +2987,6 @@ var (
 	forward_Universe_SetFederationSyncConfig_0 = runtime.ForwardResponseMessage
 
 	forward_Universe_QueryFederationSyncConfig_0 = runtime.ForwardResponseMessage
+
+	forward_Universe_IgnoreAssetOutPoint_0 = runtime.ForwardResponseMessage
 )
