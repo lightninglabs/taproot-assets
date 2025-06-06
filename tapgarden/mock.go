57df46a2af093a7e33ab1fb2c374dd92f2cbc6d7
Oliver Gugger <gugger@gmail.com>
2025-06-06 18:29:00 +0200

    multi: add DeriveSharedKey to key ring
    We'll need to be able to derive a shared key using ECDH against a wallet
    key (while supporting remote signing) for the ECIES encryption in part 4
    of the on-chain group key address support PR series.
    We can prepare for that by adding the already exising RPC method to the
    lnd key ring.

--- tapgarden/mock.go
+++ tapgarden/mock.go
@@ -972,6 +972,28 @@ func (m *MockKeyRing) ScriptKeyAt(t *testing.T, idx uint32) asset.ScriptKey {
 	})
 }
 
+func (m *MockKeyRing) DeriveSharedKey(_ context.Context, key *btcec.PublicKey,
+	locator *keychain.KeyLocator) ([32]byte, error) {
+
+	if locator == nil {
+		return [32]byte{}, fmt.Errorf("locator is nil")
+	}
+
+	m.RLock()
+	defer m.RUnlock()
+
+	priv, ok := m.Keys[*locator]
+	if !ok {
+		return [32]byte{}, fmt.Errorf("script key not found at index "+
+			"%d", locator.Index)
+	}
+
+	ecdh := &keychain.PrivKeyECDH{
+		PrivKey: priv,
+	}
+	return ecdh.ECDH(key)
+}
+
 type MockGenSigner struct {
 	KeyRing     *MockKeyRing
 	failSigning atomic.Bool
