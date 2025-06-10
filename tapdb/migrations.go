76fb0857478bd4e229238f21e95fd85cb4a3c096
ffranr <ffranr@proton.me>
2025-06-11 00:25:10 +0100

    tapdb: add foreign key refs for supply commit delegation key
    Add delegation_key_id to asset_seedlings.
    
    Replace taproot_internal_key with taproot_internal_key_id foreign key in
    mint_anchor_uni_commitments. Update related queries accordingly.
    
    Safe to drop taproot_internal_key column as supply commitment
    pre-commitment feature was not functional.
    
    The `FetchMintAnchorUniCommitment` SQL query has been extended to
    support additional filter parameters and to return multiple rows.

--- tapdb/migrations.go
+++ tapdb/migrations.go
@@ -24,7 +24,7 @@ const (
 	// daemon.
 	//
 	// NOTE: This MUST be updated when a new migration is added.
-	LatestMigrationVersion = 35
+	LatestMigrationVersion = 36
 )
 
 // DatabaseBackend is an interface that contains all methods our different
