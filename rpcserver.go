66ef737875c7cd7fdb6d7060556c33683ed6cfa9
George Tsagkarelis <george.tsagkarelis@gmail.com>
2025-06-23 14:09:32 +0200

    multi: add available rfq ids to rfqmsg.Htlc
    We add a new field to rfqmsg.Htlc which expresses the available quotes
    that may be used to send out this HTLC. This is done to allow LND to
    store an array of RFQ IDs to use later via the AuxTrafficShaper
    interface in order to query asset bandwidth and produce the correct
    asset related records for outgoing HTLCs.

--- rpcserver.go
+++ rpcserver.go
@@ -7507,7 +7507,9 @@ func (r *rpcServer) EncodeCustomRecords(_ context.Context,
 			rfqID = fn.Some[rfqmsg.ID](id)
 		}
 
-		htlc := rfqmsg.NewHtlc(assetAmounts, rfqID)
+		htlc := rfqmsg.NewHtlc(
+			assetAmounts, rfqID, fn.None[[]rfqmsg.ID](),
+		)
 
 		// We'll now map the HTLC struct into a set of TLV records,
 		// which we can then encode into the map format expected.
@@ -7649,7 +7651,9 @@ func (r *rpcServer) SendPayment(req *tchrpc.SendPaymentRequest,
 			"from peer %x with SCID %d", sellOrder.AssetAmount,
 			quote.AssetRate.String(), quote.Peer, quote.ID.Scid())
 
-		htlc := rfqmsg.NewHtlc(nil, fn.Some(quote.ID))
+		htlc := rfqmsg.NewHtlc(
+			nil, fn.Some(quote.ID), fn.None[[]rfqmsg.ID](),
+		)
 
 		// We'll now map the HTLC struct into a set of TLV records,
 		// which we can then encode into the expected map format.
@@ -7784,7 +7788,9 @@ func (r *rpcServer) SendPayment(req *tchrpc.SendPaymentRequest,
 		var rfqID rfqmsg.ID
 		copy(rfqID[:], acceptedQuote.Id)
 
-		htlc := rfqmsg.NewHtlc(nil, fn.Some(rfqID))
+		htlc := rfqmsg.NewHtlc(
+			nil, fn.Some(rfqID), fn.None[[]rfqmsg.ID](),
+		)
 
 		// We'll now map the HTLC struct into a set of TLV records,
 		// which we can then encode into the expected map format.
@@ -7859,7 +7865,9 @@ func (r *rpcServer) SendPayment(req *tchrpc.SendPaymentRequest,
 			}
 		}
 
-		htlc := rfqmsg.NewHtlc(balances, fn.None[rfqmsg.ID]())
+		htlc := rfqmsg.NewHtlc(
+			balances, fn.None[rfqmsg.ID](), fn.None[[]rfqmsg.ID](),
+		)
 
 		// We'll now map the HTLC struct into a set of TLV records,
 		// which we can then encode into the map format expected.
