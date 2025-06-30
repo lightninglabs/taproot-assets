6ead048ffc7b99c4134dfea142547e15787031e2
Oliver Gugger <gugger@gmail.com>
2025-06-30 14:50:44 +0200

    address+tapdb: addr event can have multiple outputs
    This is a preparatory commit that deals with all the database changes
    required to allow a single address receive event to have multiple asset
    outputs, and with that multiple proofs associated with it.

--- rpcserver.go
+++ rpcserver.go
@@ -3360,7 +3360,7 @@ func marshalAddrEvent(event *address.Event,
 		Outpoint:                event.Outpoint.String(),
 		UtxoAmtSat:              uint64(event.Amt),
 		ConfirmationHeight:      event.ConfirmationHeight,
-		HasProof:                event.HasProof,
+		HasProof:                event.HasAllProofs,
 	}, nil
 }
 
--- tapdb/sqlc/querier.go
+++ tapdb/sqlc/querier.go
@@ -50,6 +50,8 @@ type Querier interface {
 	DeleteUniverseSupplyRoot(ctx context.Context, namespaceRoot string) error
 	FetchAddrEvent(ctx context.Context, id int64) (FetchAddrEventRow, error)
 	FetchAddrEventByAddrKeyAndOutpoint(ctx context.Context, arg FetchAddrEventByAddrKeyAndOutpointParams) (FetchAddrEventByAddrKeyAndOutpointRow, error)
+	FetchAddrEventOutputs(ctx context.Context, addrEventID int64) ([]FetchAddrEventOutputsRow, error)
+	FetchAddrEventProofs(ctx context.Context, addrEventID int64) ([]FetchAddrEventProofsRow, error)
 	FetchAddrs(ctx context.Context, arg FetchAddrsParams) ([]FetchAddrsRow, error)
 	FetchAllNodes(ctx context.Context) ([]MssmtNode, error)
 	FetchAssetID(ctx context.Context, arg FetchAssetIDParams) ([]int64, error)
@@ -194,6 +196,8 @@ type Querier interface {
 	UpdateUTXOLease(ctx context.Context, arg UpdateUTXOLeaseParams) error
 	UpsertAddr(ctx context.Context, arg UpsertAddrParams) (int64, error)
 	UpsertAddrEvent(ctx context.Context, arg UpsertAddrEventParams) (int64, error)
+	UpsertAddrEventOutput(ctx context.Context, arg UpsertAddrEventOutputParams) (int64, error)
+	UpsertAddrEventProof(ctx context.Context, arg UpsertAddrEventProofParams) (int64, error)
 	UpsertAsset(ctx context.Context, arg UpsertAssetParams) (int64, error)
 	UpsertAssetGroupKey(ctx context.Context, arg UpsertAssetGroupKeyParams) (int64, error)
 	UpsertAssetGroupWitness(ctx context.Context, arg UpsertAssetGroupWitnessParams) (int64, error)
