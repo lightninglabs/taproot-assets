98e61a7c1591c176b11fc1913bf2b69d69bcb649
Oliver Gugger <gugger@gmail.com>
2025-06-30 14:50:41 +0200

    multi: support creating V2 addresses

--- rpcserver.go
+++ rpcserver.go
@@ -1604,17 +1604,23 @@ func (r *rpcServer) NewAddr(ctx context.Context,
 		return nil, fmt.Errorf("no proof courier address provided")
 	}
 
-	if len(req.AssetId) != 32 {
-		return nil, fmt.Errorf("invalid asset id length")
+	assetID, groupKey, err := parseAssetSpecifier(
+		req.AssetId, "", req.GroupKey, "",
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse asset specifier: %w",
+			err)
+	}
+	specifier, err := asset.NewSpecifier(assetID, groupKey, nil, true)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create asset specifier: %w",
+			err)
 	}
 
-	var assetID asset.ID
-	copy(assetID[:], req.AssetId)
+	rpcsLog.Infof("[NewAddr]: making new addr: specifier=%s, amt=%v",
+		&specifier, req.Amt)
 
-	rpcsLog.Infof("[NewAddr]: making new addr: asset_id=%x, amt=%v",
-		assetID[:], req.Amt)
-
-	err := r.checkBalanceOverflow(ctx, &assetID, nil, req.Amt)
+	err = r.checkBalanceOverflow(ctx, assetID, groupKey, req.Amt)
 	if err != nil {
 		return nil, err
 	}
@@ -1645,9 +1651,8 @@ func (r *rpcServer) NewAddr(ctx context.Context,
 		// Now that we have all the params, we'll try to add a new
 		// address to the addr book.
 		addr, err = r.cfg.AddrBook.NewAddress(
-			ctx, addrVersion, asset.NewSpecifierFromId(assetID),
-			req.Amt, tapscriptSibling, *courierAddr,
-			address.WithAssetVersion(assetVersion),
+			ctx, addrVersion, specifier, req.Amt, tapscriptSibling,
+			*courierAddr, address.WithAssetVersion(assetVersion),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("unable to make new addr: %w",
@@ -1695,9 +1700,9 @@ func (r *rpcServer) NewAddr(ctx context.Context,
 		// Now that we have all the params, we'll try to add a new
 		// address to the addr book.
 		addr, err = r.cfg.AddrBook.NewAddressWithKeys(
-			ctx, addrVersion, asset.NewSpecifierFromId(assetID),
-			req.Amt, *scriptKey, internalKey, tapscriptSibling,
-			*courierAddr, address.WithAssetVersion(assetVersion),
+			ctx, addrVersion, specifier, req.Amt, *scriptKey,
+			internalKey, tapscriptSibling, *courierAddr,
+			address.WithAssetVersion(assetVersion),
 		)
 		if err != nil {
 			return nil, fmt.Errorf("unable to make new addr: %w",
