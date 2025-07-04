ac3a63ba5a74549e74f6219f100dd2823e6afa1c
George Tsagkarelis <george.tsagkarelis@gmail.com>
2025-07-04 18:30:49 +0200

    rpcserver: return early on rejected quote
    Previously we'd never catch the event that corresponded to an RFQ
    negotiation failure, which is wrapped as an InvalidQuoteRespEvent. We
    now read that message and check if it relates to us, then return an
    error if that's the case.

--- rpcserver.go
+++ rpcserver.go
@@ -7786,10 +7786,26 @@ func (r *rpcServer) AddAssetBuyOrder(ctx context.Context,
 
 	timeout := time.After(time.Second * time.Duration(req.TimeoutSeconds))
 
+	type (
+		targetEventType = *rfq.PeerAcceptedBuyQuoteEvent
+		rejectEventType = *rfq.InvalidQuoteRespEvent
+	)
+
 	for {
-		type targetEventType = *rfq.PeerAcceptedBuyQuoteEvent
 		select {
 		case event := <-eventSubscriber.NewItemCreated.ChanOut():
+			reject, ok := event.(rejectEventType)
+			if ok {
+				if reject.QuoteResponse.MsgPeer() ==
+					*buyOrder.Peer.UnwrapToPtr() {
+
+					return nil, fmt.Errorf("peer %s "+
+						"rejected quote %v",
+						peer.String(),
+						reject.QuoteResponse.String())
+				}
+			}
+
 			acceptedQuote, ok := event.(targetEventType)
 			if !ok {
 				rpcsLog.Debugf("Received event of type %T "+
@@ -7980,10 +7996,26 @@ func (r *rpcServer) AddAssetSellOrder(ctx context.Context,
 
 	timeout := time.After(time.Second * time.Duration(req.TimeoutSeconds))
 
+	type (
+		targetEventType = *rfq.PeerAcceptedSellQuoteEvent
+		rejectEventType = *rfq.InvalidQuoteRespEvent
+	)
+
 	for {
-		type targetEventType = *rfq.PeerAcceptedSellQuoteEvent
 		select {
 		case event := <-eventSubscriber.NewItemCreated.ChanOut():
+			reject, ok := event.(rejectEventType)
+			if ok {
+				if reject.QuoteResponse.MsgPeer() ==
+					*sellOrder.Peer.UnwrapToPtr() {
+
+					return nil, fmt.Errorf("peer %s "+
+						"rejected quote %v",
+						peer.String(),
+						reject.QuoteResponse.String())
+				}
+			}
+
 			acceptedQuote, ok := event.(targetEventType)
 			if !ok {
 				rpcsLog.Debugf("Received event of type %T "+
