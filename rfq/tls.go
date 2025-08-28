2557d9d9a0b2746decbeaa71d18fa4b26752b894
Jared Tobin <jared@jtobin.io>
2025-08-28 08:16:43 -0230

    rfq: allow os, custom certificates
    Adds both 'TrustSystemRootCAs' and 'CustomCertificates' to the rfq
    TLSConfig. The former indicates whether or not to trust the operating
    system's root CA list; the latter allows additional certificates (CA or
    self-signed) to be trusted.
    
    Also adds a basic unit test skeleton.

--- rfq/tls.go
+++ rfq/tls.go
@@ -1,9 +1,28 @@
 package rfq
 
+import (
+	"crypto/tls"
+	"crypto/x509"
+
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 // TLSConfig represents TLS configuration options for oracle connections.
 type TLSConfig struct {
+	// Enabled indicates that we should use TLS.
+	Enabled bool
+
 	// InsecureSkipVerify disables certificate verification.
 	InsecureSkipVerify bool
+
+	// TrustSystemRootCAs indicates whether or not to use the operating
+	// system's root certificate authority list.
+	TrustSystemRootCAs bool
+
+	// CustomCertificates contains PEM data for additional root CA and
+	// self-signed certificates to trust.
+	CustomCertificates []byte
 }
 
 // DefaultTLSConfig returns a default TLS configuration.
@@ -11,4 +30,46 @@ func DefaultTLSConfig() *TLSConfig {
 	return &TLSConfig{
 		InsecureSkipVerify: true,
 	}
+}
+
+// configureTransportCredentials configures the TLS transport credentials to
+// be used for RPC connections.
+func configureTransportCredentials(
+	config *TLSConfig) (credentials.TransportCredentials, error) {
+
+	// If TLS is disabled, return insecure credentials.
+	if !config.Enabled {
+		return insecure.NewCredentials(), nil
+	}
+
+	// If we're to skip certificate verification, then return TLS
+	// credentials with certificate verification disabled.
+	if config.InsecureSkipVerify {
+		creds := credentials.NewTLS(&tls.Config{
+			InsecureSkipVerify: true,
+		})
+		return creds, nil
+	}
+
+	// Initialize the certificate pool.
+	certPool, err := constructCertPool(config.TrustSystemRootCAs)
+	if err != nil {
+		return nil, err
+	}
+
+	// If we have any custom certificates, add them to the certificate
+	// pool.
+	certPool.AppendCertsFromPEM(config.CustomCertificates)
+
+	// Return the constructed transport credentials.
+	return credentials.NewClientTLSFromCert(certPool, ""), nil
+}
+
+// constructCertPool is a helper for constructing an initial certificate pool,
+// depending on whether or not we should trust the system root CA list.
+func constructCertPool(trustSystem bool) (*x509.CertPool, error) {
+	if trustSystem {
+		return x509.SystemCertPool()
+	}
+	return x509.NewCertPool(), nil
 }
