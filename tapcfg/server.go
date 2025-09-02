7186b8f62456ef67f5687a3bc2df54082943548a
George Tsagkarelis <george.tsagkarelis@gmail.com>
2025-09-02 13:07:50 +0200

    tapchannel: include stxo proofs on channel funding
    This commit updates the aux funding controller to conditionally use stxo
    proofs, depending on whether the feature bit was negotiated or not with
    the remote peer. Our funding commitment will now include the alt leaves
    for stxos if the flag is set.
    
    The funder needs to construct the correct tapscript early on, as LND
    will query the tapscript root before we reach the finalization phase.
    That's why we manually calculate and merge the stxo alt leaves in the
    funding process.
    
    The fundee now also needs to calculate and merge the alt leaves that
    result from the asset outputs, in order to arrive to the same tapscript
    root.

--- tapcfg/server.go
+++ tapcfg/server.go
@@ -588,6 +588,7 @@ func genServerConfig(cfg *Config, cfgLogger btclog.Logger,
 			AssetSyncer:        addrBook,
 			FeatureBits:        lndFeatureBitsVerifier,
 			IgnoreChecker:      ignoreCheckerOpt,
+			AuxChanNegotiator:  auxChanNegotiator,
 			ErrChan:            mainErrChan,
 		},
 	)
