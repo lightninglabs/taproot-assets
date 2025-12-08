20f0c9b528c72b6d53beff3b5db4a20354422b0f
George Tsagkarelis <george.tsagkarelis@gmail.com>
2025-12-08 14:51:08 +0000

    Merge pull request #1899 from lightninglabs/wip/improve-htlc-intercept-setup-error-handling
    Improve HTLC interceptor setup handling and fix itest flake
    
    (cherry picked from commit cfee1c80cca404c4e12548e008f2e247b9c24c7d)

--- rfq/manager.go
+++ rfq/manager.go
@@ -264,6 +264,7 @@ func (m *Manager) startSubsystems(ctx context.Context) error {
 		SpecifierChecker:  m.AssetMatchesSpecifier,
 		NoOpHTLCs:         m.cfg.NoOpHTLCs,
 		AuxChanNegotiator: m.cfg.AuxChanNegotiator,
+		ErrChan:           m.subsystemErrChan,
 	})
 	if err != nil {
 		return fmt.Errorf("error initializing RFQ order handler: %w",
