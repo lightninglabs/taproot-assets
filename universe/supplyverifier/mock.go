1f613b99459119e2c3b76d2a4920403efa21bde4
rxbryan <rxbryn@gmail.com>
2026-04-10 23:46:12 +0100

    supplyverifier: add MockSupplyTreeView and MockGroupFetcher

--- universe/supplyverifier/mock.go
+++ universe/supplyverifier/mock.go
@@ -3,8 +3,11 @@ package supplyverifier
 import (
 	"context"
 
+	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightninglabs/taproot-assets/asset"
+	"github.com/lightninglabs/taproot-assets/fn"
+	"github.com/lightninglabs/taproot-assets/mssmt"
 	"github.com/lightninglabs/taproot-assets/universe/supplycommit"
 	lfn "github.com/lightningnetwork/lnd/fn/v2"
 	"github.com/stretchr/testify/mock"
@@ -73,4 +76,52 @@ func (m *MockSupplyCommitView) InsertSupplyCommit(ctx context.Context,
 
 	args := m.Called(ctx, assetSpec, commit, leaves, unspentPreCommits)
 	return args.Error(0)
+}
+
+// MockSupplyTreeView is a mock implementation of the SupplyTreeView interface.
+type MockSupplyTreeView struct {
+	mock.Mock
+}
+
+func (m *MockSupplyTreeView) FetchSupplyTrees(ctx context.Context,
+	spec asset.Specifier) (mssmt.Tree, *supplycommit.SupplyTrees, error) {
+
+	args := m.Called(ctx, spec)
+	if args.Get(0) == nil {
+		return nil, nil, args.Error(2)
+	}
+	return args.Get(0).(mssmt.Tree),
+		args.Get(1).(*supplycommit.SupplyTrees),
+		args.Error(2)
+}
+
+func (m *MockSupplyTreeView) FetchSubTrees(ctx context.Context,
+	assetSpec asset.Specifier,
+	blockHeightEnd fn.Option[uint32]) lfn.Result[supplycommit.SupplyTrees] {
+
+	args := m.Called(ctx, assetSpec, blockHeightEnd)
+	return args.Get(0).(lfn.Result[supplycommit.SupplyTrees])
+}
+
+func (m *MockSupplyTreeView) FetchSupplyLeavesByHeight(ctx context.Context,
+	spec asset.Specifier,
+	startHeight, endHeight uint32) lfn.Result[supplycommit.SupplyLeaves] {
+
+	args := m.Called(ctx, spec, startHeight, endHeight)
+	return args.Get(0).(lfn.Result[supplycommit.SupplyLeaves])
+}
+
+// MockGroupFetcher is a mock implementation of tapgarden.GroupFetcher.
+type MockGroupFetcher struct {
+	mock.Mock
+}
+
+func (m *MockGroupFetcher) FetchGroupByGroupKey(ctx context.Context,
+	groupKey *btcec.PublicKey) (*asset.AssetGroup, error) {
+
+	args := m.Called(ctx, groupKey)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*asset.AssetGroup), args.Error(1)
 }
